21a7e706c834542a9ed0677b57b04dbc1ed12f54
Boris Pek <tehnick-8@mail.ru>
2010-03-28 22:56:10 +0000

    расширены настройки для логов

--- eiskaltdcpp/SettingsLog.cpp
+++ eiskaltdcpp/SettingsLog.cpp
@@ -21,15 +21,19 @@ void SettingsLog::init(){
 
     groupBox_MAINCHAT->setChecked(BOOLSETTING(LOG_MAIN_CHAT));
     lineEdit_CHATFMT->setText(_q(SETTING(LOG_FORMAT_MAIN_CHAT)));
+    lineEdit_FILE_CHATFMT->setText(_q(SETTING(LOG_FILE_MAIN_CHAT)));
 
     groupBox_PM->setChecked(BOOLSETTING(LOG_PRIVATE_CHAT));
     lineEdit_PMFMT->setText(_q(SETTING(LOG_FORMAT_PRIVATE_CHAT)));
+    lineEdit_FILE_PMFMT->setText(_q(SETTING(LOG_FILE_PRIVATE_CHAT)));
 
     groupBox_DOWN->setChecked(BOOLSETTING(LOG_DOWNLOADS));
     lineEdit_DOWNFMT->setText(_q(SETTING(LOG_FORMAT_POST_DOWNLOAD)));
+    lineEdit_FILE_DOWNFMT->setText(_q(SETTING(LOG_FILE_DOWNLOAD)));
 
     groupBox_UP->setChecked(BOOLSETTING(LOG_UPLOADS));
     lineEdit_UPFMT->setText(_q(SETTING(LOG_FORMAT_POST_UPLOAD)));
+    lineEdit_FILE_UPFMT->setText(_q(SETTING(LOG_FILE_UPLOAD)));
 
     checkBox_FILELIST->setChecked(BOOLSETTING(LOG_SYSTEM));
     checkBox_STAT->setChecked(BOOLSETTING(LOG_STATUS_MESSAGES));
@@ -50,12 +54,16 @@ void SettingsLog::ok(){
     sm->set(SettingsManager::LOG_DIRECTORY, _tq(path));
     sm->set(SettingsManager::LOG_MAIN_CHAT, groupBox_MAINCHAT->isChecked());
     sm->set(SettingsManager::LOG_FORMAT_MAIN_CHAT, _tq(lineEdit_CHATFMT->text()));
+    sm->set(SettingsManager::LOG_FILE_MAIN_CHAT, _tq(lineEdit_FILE_CHATFMT->text()));
     sm->set(SettingsManager::LOG_PRIVATE_CHAT, groupBox_PM->isChecked());
     sm->set(SettingsManager::LOG_FORMAT_PRIVATE_CHAT, _tq(lineEdit_PMFMT->text()));
+    sm->set(SettingsManager::LOG_FILE_PRIVATE_CHAT, _tq(lineEdit_FILE_PMFMT->text()));
     sm->set(SettingsManager::LOG_DOWNLOADS, groupBox_DOWN->isChecked());
     sm->set(SettingsManager::LOG_FORMAT_POST_DOWNLOAD, _tq(lineEdit_DOWNFMT->text()));
+    sm->set(SettingsManager::LOG_FILE_DOWNLOAD, _tq(lineEdit_FILE_DOWNFMT->text()));
     sm->set(SettingsManager::LOG_UPLOADS, groupBox_UP->isChecked());
     sm->set(SettingsManager::LOG_FORMAT_POST_UPLOAD, _tq(lineEdit_UPFMT->text()));
+    sm->set(SettingsManager::LOG_FILE_UPLOAD, _tq(lineEdit_FILE_UPFMT->text()));
     sm->set(SettingsManager::LOG_SYSTEM, checkBox_SYSTEM->isChecked());
     sm->set(SettingsManager::LOG_STATUS_MESSAGES, checkBox_STAT->isChecked());
     sm->set(SettingsManager::LOG_FILELIST_TRANSFERS, checkBox_FILELIST->isChecked());
