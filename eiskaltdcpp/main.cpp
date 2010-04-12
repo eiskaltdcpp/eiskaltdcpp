e05bc0c0603926141cf71e43ce3d6bd8476b5381
Boris Pek <tehnick-8@mail.ru>
2010-04-12 17:53:54 +0000

    issue #256 (начало)

--- eiskaltdcpp/main.cpp
+++ eiskaltdcpp/main.cpp
@@ -102,11 +102,15 @@ int main(int argc, char *argv[])
     Notification::getInstance()->enableTray(WBGET(WB_TRAY_ENABLED));
 
     MainWindow::getInstance()->autoconnect();
-    MainWindow::getInstance()->show();
-    MainWindow::getInstance()->parseCmdLine();
 
-    if (WBGET(WB_MAINWINDOW_HIDE))
-        MainWindow::getInstance()->hide();
+    if (!WBGET(WB_MAINWINDOW_HIDE) ||
+        !WBGET(WB_TRAY_ENABLED) ||
+        !QSystemTrayIcon::isSystemTrayAvailable())
+    {
+        MainWindow::getInstance()->show();
+    }
+
+    MainWindow::getInstance()->parseCmdLine();
 
     ret = app.exec();
 
