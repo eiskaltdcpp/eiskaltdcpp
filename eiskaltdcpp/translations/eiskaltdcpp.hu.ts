59f3bed12e357e381c08b752dfebad5426672674
Akos Berki <husumo@gmail.com>
2010-04-21 21:34:09 +0000

    Hungarian translation update

--- eiskaltdcpp/translations/eiskaltdcpp.hu.ts
+++ eiskaltdcpp/translations/eiskaltdcpp.hu.ts
@@ -1264,7 +1264,7 @@ Kérlek ellenőrizd a kapcsolat beállításokat</translation>
     </message>
     <message>
         <source>&lt;b&gt;Speed&lt;/b&gt;&lt;br/&gt;Download: &lt;font_color=&quot;green&quot;&gt;%1&lt;/font&gt; Upload: &lt;font_color=&quot;red&quot;&gt;%2&lt;/font&gt;&lt;br/&gt;&lt;b&gt;Statistics&lt;/b&gt;&lt;br/&gt;Downloaded: &lt;font_color=&quot;green&quot;&gt;%3&lt;/font&gt; Uploaded: &lt;font_color=&quot;red&quot;&gt;%4&lt;/font&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;Sebesség&lt;/b&gt;&lt;br/&gt;Letöltés: &lt;font_color=&quot;green&quot;&gt;%1&lt;/font&gt; Feltöltés: &lt;font_color=&quot;red&quot;&gt;%2&lt;/font&gt;&lt;br/&gt;&lt;b&gt;Statisztika&lt;/b&gt;&lt;br/&gt;Letöltve: &lt;font_color=&quot;green&quot;&gt;%3&lt;/font&gt; Feltöltve: &lt;font_color=&quot;red&quot;&gt;%4&lt;/font&gt;</translation>
     </message>
 </context>
 <context>
