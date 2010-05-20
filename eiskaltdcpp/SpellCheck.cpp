a9aaea6ce1454f5fb939ac276c5894858b6b8f46
Andrey Karlov <dein.negativ@gmail.com>
2010-05-20 17:27:03 +0000

    some fixes for aspell

--- eiskaltdcpp/SpellCheck.cpp
+++ eiskaltdcpp/SpellCheck.cpp
@@ -1,8 +1,12 @@
 #include "SpellCheck.h"
 #include "WulforSettings.h"
+#include "dcpp/stdinc.h"
+#include "dcpp/DCPlusPlus.h"
+#include "dcpp/Util.h"
 
 #include <QLocale>
 #include <QDir>
+#include <QtDebug>
 
 SpellCheck::SpellCheck(QObject *parent) :
     QObject(parent),
@@ -12,29 +16,9 @@ SpellCheck::SpellCheck(QObject *parent) :
     config = new_aspell_config();
 
     aspell_config_replace(config, "encoding", "utf-8");
-    aspell_config_replace(config, "personal", (QDir::homePath()+QDir::separator()+".eiskaltdc++"+QDir::separator()+"dict").toAscii().constData());
+    aspell_config_replace(config, "personal", (dcpp::Util::getPath(dcpp::Util::PATH_USER_CONFIG)+"dict").c_str());
 
     if (config){
-        /*const AspellDictInfoList *dicts = get_aspell_dict_info_list(config);
-        AspellDictInfoEnumeration *enumer = aspell_dict_info_list_elements(dicts);
-        const AspellDictInfo *info = NULL;
-
-        QStringList all;
-
-        while ((info = aspell_dict_info_enumeration_next(enumer)) != NULL)
-            all.append(QString::fromUtf8(info->code, strlen(info->code)));
-
-        if (WSGET(WS_APP_ASPELL_LANG).isEmpty()){
-            QString lc_prefix = QLocale::system().name();
-
-            if (all.contains(lc_prefix))//Loading dictionary from system locale
-                aspell_config_replace(config, "lang", lc_prefix.toAscii().constData());
-            else if (all.contains(lc_prefix.left(lc_prefix.indexOf("_")))) {
-                aspell_config_replace(config, "lang", lc_prefix.left(lc_prefix.indexOf("_")).toAscii().constData());
-            }
-        }
-        else
-            aspell_config_replace(config, "lang", WSGET(WS_APP_ASPELL_LANG).toAscii().constData());*/
         AspellCanHaveError *error = new_aspell_speller(config);
 
         if (aspell_error(error) != 0){
@@ -52,6 +36,9 @@ SpellCheck::SpellCheck(QObject *parent) :
 SpellCheck::~SpellCheck(){
     if (spell_checker)
         aspell_speller_save_all_word_lists(spell_checker);
+
+    if (config)
+        WSSET(WS_APP_ASPELL_LANG, aspell_config_retrieve(config, "lang"));
 
     delete_aspell_config(config);
     delete_aspell_speller(spell_checker);
