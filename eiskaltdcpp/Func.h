b67fc9744d52081089ccb0ddc31c9067180058ff
Boris Pek <tehnick-8@mail.ru>
2010-06-02 19:26:13 +0000

    issue #534

--- eiskaltdcpp/Func.h
+++ eiskaltdcpp/Func.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2004-2008 Jens Oknelid, paskharen@gmail.com
+ * Copyright (C) 2004-2008 Jens Oknelid, paskharen@gmail.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
