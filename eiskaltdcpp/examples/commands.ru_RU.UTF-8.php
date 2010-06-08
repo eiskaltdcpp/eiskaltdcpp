a1f5b90deeb9178bd79f76c61bd9d331ae1de490
Boris Pek <tehnick-8@mail.ru>
2010-06-08 21:20:05 +0000

    issue #344

--- eiskaltdcpp/examples/commands.ru_RU.UTF-8.php
+++ eiskaltdcpp/examples/commands.ru_RU.UTF-8.php
@@ -1,28 +1,38 @@
 #!/usr/bin/php5
 <?php
-#--------------------------------------------------#
-#--------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
 #
 # EiskaltDC++ - personal info script.
 #
-#--------------------------------------------------#
+#------------------------------------------------------------------------------#
+#
+# Поддерживыемые проигрыватели:
+#
+# Видео: totem, smplayer, vlc, gnome-mplayer, xine
+# Аудио: rhythmbox, audacious2, amarok
+#
+#------------------------------------------------------------------------------#
 # Author:  GoTLiuM InSPiRiT <gotlium@gmail.com>
-# Date:    28.05.2010
+# Date:    08.06.2010
 # License: GPL-2 (http://opensource.org/licenses/gpl-2.0.php)
-#--------------------------------------------------#
-#--------------------------------------------------#
+#------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------#
 
 
 /**
 * Примеры использования
 *
-* Добавьте псевдонимы:
+* Установка в Debian и Ubuntu (если php еще не был установлен):
+* $ sudo apt-get install php5-cli
+*
+* Добавьте псевдонимы (команды в чате):
 * /alias p::/sh  /usr/share/eiskaltdcpp/commands.ru_RU.UTF-8.php
 * /alias u::/sh  /usr/share/eiskaltdcpp/commands.ru_RU.UTF-8.php uptime
 * /alias b::/sh  /usr/share/eiskaltdcpp/commands.ru_RU.UTF-8.php black
 * /alias r::/sh  /usr/share/eiskaltdcpp/commands.ru_RU.UTF-8.php ratio
-* /alias ps::/sh /usr/share/eiskaltdcpp/commands.ru_RU.UTF-8.php process
 * /alias t::/sh  /usr/share/eiskaltdcpp/commands.ru_RU.UTF-8.php torrent
+* /alias ps::/sh /usr/share/eiskaltdcpp/commands.ru_RU.UTF-8.php process
 * /alias pn::/sh /usr/share/eiskaltdcpp/commands.ru_RU.UTF-8.php ping
 * /alias ip::/sh /usr/share/eiskaltdcpp/commands.ru_RU.UTF-8.php ip
 *
@@ -35,25 +45,28 @@
 * /ps - системная информация
 * /pn - пингуем сайт mail.ru
 * /ip - ваш ip адрес
+*
+* Проверить список доступных псевдонимов можно так:
+* /alias list
 **/
 
 
 final class EiskaltDC
 {
-  // имя системного пользователя
+  // имя системного пользователя (заполнять не требуется)
   private $usename = '';
 
-  // смайлики, которые используем
+  // смайлики, которые используем (здесь можно указать коды любимых смайлов)
   private $smiles = array(
     ':дэнс:', ':diablored:', ':diabloblack:', ':Кенни:',
     ':нинзя:', ':бубльгум:', '}:-Df>', ':смерть:',
     ':роджер:', ':рокер:', ':гитарист:'
   );
 
-  // переменная содержащая ошибки
+  // переменная содержащая ошибки (заполнять не требуется)
   public  $errors  = array();
 
-  /**
+/**
    * Метод по умолчанию
    *
    * @access public
@@ -61,7 +74,10 @@ final class EiskaltDC
    */
   public function __construct()
   {
-    if( function_exists('exec') && stristr(PHP_OS, 'lin') )
+    if( version_compare(PHP_VERSION, '5.2.0', '<') ) {
+      $this->errors[] = null;
+    }
+    elseif( function_exists('exec') && stristr(PHP_OS, 'lin') )
     {
       exec('whoami', $username);
 
@@ -153,76 +169,129 @@ final class EiskaltDC
 
 
   /**
-   * То что смотрим в Totem или слушаем в Rhythmbox
+   * То что смотрим в видео проигрывателе или слушаем в аудио
    *
    * @access public
    * @return string
    */
   public function play()
   {
-    exec("ps -e | grep totem | awk '{print $1}'", $ps_tm);
+    // имена процессов видео-проигрывателей
+    $vplayers = array('totem', 'smplayer', 'vlc', 'gnome-mplayer', 'xine');
 
-    if( is_array($ps_tm) && count($ps_tm) == 1 && is_numeric($ps_tm[0]) )
+    // bash команды, для извлечения заголовков
+    $get_title[] = 'id=`xdotool search --class totem | head -n 1` && xprop -id $id | grep "WM_ICON_NAME" | head -n 1 | sed -n \'s/.*"\(.*\)\"/\1/p\'';
+    $get_title[] = 'id=`xdotool search --class smplayer | head -n 1` && xprop -id $id | grep "WM_NAME(STRING)" | head -n 1 | sed -n \'s/.*"\(.*\)\s-\sSMPlayer"/\1/p\'';
+    $get_title[] = 'id=`xdotool search --class vlc | head -n 1` && xprop -id $id | grep "WM_NAME(COMPOUND_TEXT)" | head -n 1 | sed -n \'s/.*"\(.*\)\-\(.*\)\-\(.*\)VLC\"/\1/p\'';
+    $get_title[] = 'id=`xdotool search --class gnome-mplayer | head -n 1` && xprop -id $id | grep "WM_ICON_NAME" | head -n 1 | sed -n \'s/.*"\(.*\)\s-\sGNOME\(.*\)\"/\1/p\'';
+    $get_title[] = 'id=`xdotool search --class xine | head -n 1` && xprop -id $id | grep "WM_ICON_NAME" | head -n 1 | sed -n \'s/.*"\(.*\)\s-\s\(.*\)\"/\2/p\'';
+
+    // проверка наличия процессов. если имеется хоть один,
+    // то извлекается заголовок и соответственно в чат.
+    foreach( $vplayers as $id => $player )
     {
-      exec('id=`xdotool search --class totem | head -n 1` && xprop -id $id | grep "WM_ICON_NAME" | head -n 1', $win_title);
+      exec("ps -e | grep $player | awk '{print $1}'", $ps_tm);
 
-      if( is_array($win_title) && count($win_title) == 1 && !empty($win_title) )
+      if( is_array($ps_tm) && count($ps_tm) == 1 && is_numeric($ps_tm[0]) )
       {
-        preg_replace('#\=[\s]"(.*?)"#e', '$title="$1";', $win_title[0]);
+        exec($get_title[$id], $win_title);
 
-        if(!empty($title)) {
-          return 'смотрю что-то под названием: '.str_replace('"', '', $title) . ' :ТВ:';
-        }
-      }
-    }
-
-    exec("ps -e | grep rhythmbox | awk '{print $1}'", $ps);
-
-    if( is_array($ps) && count($ps) == 1 && is_numeric($ps[0]) )
-    {
-
-      exec("rhythmbox-client --print-volume | awk '{print $5}'", $vol);
-
-      $volume = substr($vol[0],0,(strlen($vol[0])-1));
-      $volume = str_replace(',','.',$volume);
-      $volume = $volume * 100;
-      $volume = number_format($volume);
-
-      exec("echo `rhythmbox-client --print-playing-format \"%aa,%at,%ay,%tt,%td\"`",$info);
-      exec("amixer sget Master | grep \"Mono:\" | awk '{print $4}'",$sysvol);
-      if( empty($sysvol[0]) ) {
-        $sysvol = array();
-        exec("amixer sget Master | grep \"Front Left:\" | awk '{print $5}'",$sysvol);
-      }
-      preg_replace('#\[(.*)\]#e', '$sysvol="$1";', $sysvol[0]);
-
-      $info = explode(',', $info[0]);
-      $info_head = array('группой','альбом','год','песня','время');
-      $out = array();
-
-      foreach( $info as $i => $value )
-      {
-        $value = trim($value);
-
-        if( $value != 'Неизвестно' && $value != 'Unknown' && $value != '0' && !empty($value) && preg_match('#[a-zа-я]#iu',$value) )
+        if( is_array($win_title) && count($win_title) == 1 && !empty($win_title[0]) )
         {
-          $out[] = "{$info_head[$i]}: $value";
+          if( preg_match('#[a-zа-я]#iu',$win_title[0]) )
+          {
+            return 'смотрю что-то под названием: '.trim($win_title[0]).' :ТВ:';
+          }
         }
+
+        unset($win_title);
       }
 
-      if( is_array($out) && count($out) > 1 )
+      unset($ps_tm);
+    }
+
+    unset( $id, $player );
+
+    // Уровень системного звука
+    exec("amixer sget Master | grep \"Mono:\" | awk '{print $4}'",$sysvol);
+
+    if( empty($sysvol[0]) ) {
+      $sysvol = array();
+      exec("amixer sget Master | grep \"Front Left:\" | awk '{print $5}'",$sysvol);
+    }
+
+    if( is_array($sysvol) && !empty($sysvol[0]) ) {
+      preg_replace('#\[(.*)\]#e', '$sys_volume="$1";', $sysvol[0]);
+    }
+
+    unset($sysvol);
+    //
+
+
+    // имена процессов аудио-проигрывателей
+    $aplayers = array('rhythmbox', 'audacious2', 'amarok');
+
+    // bash команды
+    $get_data[] = 'echo `rhythmbox-client --print-playing-format %aa,%at,%ay,%tt,%td`';
+    $get_data[] = 'audacious=`php -r \'exec("audtool2 current-song && audtool2 current-song-bitrate-kbps",$aud);$tg="/usr/share/php-getid3/getid3.php";$f=false;if(file_exists($tg)){include $tg;exec("audtool2 current-song-filename",$file);$getID3 = new getID3;$info = $getID3->analyze($file[0]);if(isset($info["id3v1"])){print $info["id3v1"]["artist"].",".$info["id3v1"]["album"].",".$info["id3v1"]["year"].",".$info["id3v1"]["title"].",,{$aud[1]}";}else{$f=true;}}else{$f=true;}if($f==true){print",,,{$aud[0]},,{$aud[1]}";}\'` && echo $audacious';
+    $get_data[] = 'amarock=`qdbus org.kde.amarok /Player org.freedesktop.MediaPlayer.GetMetadata 2>/dev/null` && artist=`echo "$amarock" | sed -ne \'s/^artist: \(.*\)$/\1/p\'` && album=`echo "$amarock" | sed -ne \'s/^album: \(.*\)$/\1/p\'` && year=`echo "$amarock" | sed -ne \'s/^year: \(.*\)$/\1/p\'` && title=`echo "$amarock" | sed -ne \'s/^title: \(.*\)$/\1/p\'` && bitrate=`echo "$amarock" | sed -ne \'s/^audio\-bitrate: \(.*\)$/\1/p\'` && echo "$artist,$album,$year,$title,,$bitrate"';
+
+    // используемые поля
+    $info_head = array('группой','альбом','год','песня','время','bitrate');
+
+    foreach( $aplayers as $id => $player )
+    {
+      exec("ps -e | grep $player | awk '{print $1}'", $ps);
+
+      if( is_array($ps) && count($ps) == 1 && is_numeric($ps[0]) )
       {
-        shuffle($this->smiles);
-        $format = implode(', ', $out);
-        return 'наслаждаюсь '.implode(', ', $out).', уровень громкости: (rhythmbox:'.$volume.'% / system:'.$sysvol.') ' . current($this->smiles);
+        exec($get_data[$id], $win_title);
+
+        if( is_array($win_title) && !empty($win_title[0]) )
+        {
+          $info = explode(',', $win_title[0]);
+          $out = array();
+
+          if( is_array($info) && count($info) > 0 ):
+
+          foreach( $info as $i => $value )
+          {
+            $value = trim($value);
+
+            if( $value != 'Неизвестно' && $value != 'Unknown' && $value != '0' && !empty($value) && preg_match('#[a-zа-я0-9]#iu',$value) )
+            {
+              $out[] = "{$info_head[$i]}: $value";
+            }
+          }
+
+          if( is_array($out) && count($out) > 1 )
+          {
+            if( $player == 'rhythmbox' ):
+            exec("rhythmbox-client --print-volume | awk '{print $5}'", $vol);
+            $volume = substr($vol[0],0,(strlen($vol[0])-1));
+            $volume = str_replace(',','.',$volume);
+            $volume = $volume * 100;
+            $volume = number_format($volume);
+            endif;
+
+            shuffle($this->smiles);
+            $format = implode(', ', $out);
+            $volume = (!empty($volume)) ? 'уровень громкости: (player:'.$volume.'% / system:'.$sys_volume.')': "уровень сис. громкости: $sys_volume";
+
+            return 'наслаждаюсь '.implode(', ', $out).", $volume " . current($this->smiles);
+          }
+
+          endif;
+
+        }
+
+        unset($win_title, $out);
       }
-      else {
-        return 'enjoy the silence... :слушаю:';
-      }
+
+      unset($ps);
     }
-    else {
-      return 'enjoy the silence... :слушаю:';
-    }
+
+    return 'enjoy the silence... :слушаю:';
   }
 
 
@@ -378,7 +447,8 @@ final class EiskaltDC
 
 
   /**
-   * IP адрес интерфейса ppp0
+   * IP адрес интерфейса ppp0 или если прокинули
+   * порты, с checkip сервиса
    *
    * @access public
    * @return string
@@ -388,7 +458,7 @@ final class EiskaltDC
     exec('ifconfig | grep -A 1 ppp0 | grep inet | cut -d ":" -f2 | cut -d " " -f1', $ip);
 
     if( is_array($ip) && count($ip) == 1 ) {
-      return "my ip: {$ip[0]} d'dos-те меня, мне пох! :ПиСи:";
+      return "my ip: {$ip[0]} d'dos-те меня, мне всё равно! :ПиСи:";
     } else {
       $f=@fopen('http://checkip.dyndns.com/','rb');
       if( $f ) {
@@ -409,12 +479,16 @@ final class EiskaltDC
     }
   }
 
+
 }
 
 $method  = (isset($argv[1])) ? trim($argv[1]) : 'play';
 $eiskalt = new EiskaltDC;
 
-if( !count($eiskalt->errors) && method_exists($eiskalt, $method) ) {
+if( count($eiskalt->errors) > 0 ) {
+  print "php v.5.2 <, foo 'exec' not exists or os is not linux!";
+}
+elseif( method_exists($eiskalt, $method) ) {
   $str = $eiskalt->$method();
   print (!count($eiskalt->errors)) ? "+me -> $str\n" : "error result\n";
 } else {
