540aa7b96315144e4f4257f43f4c74e6749fd0f9
Boris Pek <tehnick-8@mail.ru>
2010-07-10 22:35:20 +0000

    изменения для сборки под MS Windows (продолжение)

--- win32/variables.bat
+++ win32/variables.bat
@@ -2,10 +2,10 @@ set QTSDKDIR="%SystemDrive%\Qt\2010.01"
 set CMAKEDIR="%ProgramFiles%\CMake 2.8"
 set TARGET="%ProgramFiles%\EiskaltDC++"
 
-REM For example:
-REM set QTSDKDIR="C:\Qt\2010.01"
-REM set CMAKEDIR="C:\Program Files\CMake 2.8"
-REM set TARGET="C:\Program Files\EiskaltDC++"
+@REM For example:
+@REM set QTSDKDIR="C:\Qt\2010.01"
+@REM set CMAKEDIR="C:\Program Files\CMake 2.8"
+@REM set TARGET="C:\Program Files\EiskaltDC++"
 
 set MINGW=%QTSDKDIR%\mingw
 set QTDIR=%QTSDKDIR%\qt
