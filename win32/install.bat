330ce1e91b14b9812f72d978982deec43c9947a4
Boris Pek <tehnick-8@mail.ru>
2010-07-11 12:31:37 +0000

    изменения для сборки под MS Windows (продолжение)

--- win32/install.bat
+++ win32/install.bat
@@ -5,14 +5,16 @@ mingw32-make -k install
 strip %TARGET%\EiskaltDC++.exe
 
 
-copy /Y "%QTDIR%\bin\QtCore4.dll"  %TARGET%
-copy /Y "%QTDIR%\bin\QtGui4.dll"   %TARGET%
-copy /Y "%QTDIR%\bin\QtNetwork4.dll" %TARGET%
-copy /Y "%QTDIR%\bin\QtXml4.dll" %TARGET%
+copy /Y "dcppboot.xml"                   %TARGET%
 
-copy /Y "%MINGW%\bin\mingwm10.dll" %TARGET%
-copy /Y "%MINGW%\bin\intl.dll"     %TARGET%
-copy /Y "%MINGW%\bin\iconv.dll"    %TARGET%
-copy /Y "%MINGW%\bin\mgwz.dll"     %TARGET%
-copy /Y "%MINGW%\bin\mgwbz2-1.dll" %TARGET%
+copy /Y "%QTDIR%\bin\QtCore4.dll"        %TARGET%
+copy /Y "%QTDIR%\bin\QtGui4.dll"         %TARGET%
+copy /Y "%QTDIR%\bin\QtNetwork4.dll"     %TARGET%
+copy /Y "%QTDIR%\bin\QtXml4.dll"         %TARGET%
+
+copy /Y "%MINGW%\bin\mingwm10.dll"       %TARGET%
+copy /Y "%MINGW%\bin\intl.dll"           %TARGET%
+copy /Y "%MINGW%\bin\iconv.dll"          %TARGET%
+copy /Y "%MINGW%\bin\mgwz.dll"           %TARGET%
+copy /Y "%MINGW%\bin\mgwbz2-1.dll"       %TARGET%
 copy /Y "%MINGW%\bin\libgcc_s_dw2-1.dll" %TARGET%
