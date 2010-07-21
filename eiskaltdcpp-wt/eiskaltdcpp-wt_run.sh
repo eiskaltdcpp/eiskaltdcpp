c0de38e7825aa4d53fcbedb247f29210a0e25aa0
Boris Pek <tehnick-8@mail.ru>
2010-07-21 21:59:46 +0000

    cmake up for eiskaltdcpp-wt

--- eiskaltdcpp-wt/eiskaltdcpp-wt_run.sh
+++ eiskaltdcpp-wt/eiskaltdcpp-wt_run.sh
@@ -5,5 +5,5 @@ export ADDRESS=0.0.0.0
 
 echo "Launching eiskaltdcpp-wt..."
 eiskaltdcpp-wt --http-port=$PORT --http-address=$ADDRESS --docroot /usr/share/eiskaltdcpp/wt/
-echo "eiskaltdcpp-wt was started on $ADDRESS:$PORT"
+echo "Quit..."
 
