488f1315a8629c3552440a23b0c9ccd3cf009230
Eugene Petrov <dhamp@ya.ru>
2010-08-10 19:20:42 +0000

    doc up

---
+++ examples/audacious.en_US.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# This script shows that are now played in Audacious
+
+## Author: none
+## License: Public Domain
+
+## Depends: sh, audacious
+
+## Examples:
+# output in console:
+# /me Audacious playing now: Rammstein - Sonne
+# output in eiskaltdcpp chat:
+# [10:57:03] * Ben_Vladen Audacious playing now: Rammstein - Sonne
+
+au="$(audtool2 --current-song)"
+echo "/me Audacious playing now: ${au}"
