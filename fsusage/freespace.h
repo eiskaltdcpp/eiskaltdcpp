2f525805b7ea2fcfae94ca512306df0fe79c974d
Eugene Petrov <dhamp@ya.ru>
2010-08-13 20:57:09 +0000

    fix fsusage

--- fsusage/freespace.h
+++ fsusage/freespace.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <dcpp/stdinc.h>
+#include <string>
 struct FreeSpace {
         static bool FreeDiscSpace ( std::string path, unsigned long long * res, unsigned long long * res2);
 };
+
