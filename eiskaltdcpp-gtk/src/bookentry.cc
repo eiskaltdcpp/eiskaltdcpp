49052b00ed271a773aef9685d61376927488755a
Eugene Petrov <dhamp@ya.ru>
2010-09-09 08:12:02 +0000

    gtk gui: fix icon on tab

--- eiskaltdcpp-gtk/src/bookentry.cc
+++ eiskaltdcpp-gtk/src/bookentry.cc
@@ -28,193 +28,193 @@ using namespace std;
 GSList* BookEntry::group = NULL;
 
 BookEntry::BookEntry(const EntryType type, const string &text, const string &glade, const string &id):
-	Entry(type, glade, id),
-	bold(FALSE),
-	urgent(FALSE)
+    Entry(type, glade, id),
+    bold(FALSE),
+    urgent(FALSE)
 {
-	labelBox = gtk_hbox_new(FALSE, 5);
+    labelBox = gtk_hbox_new(FALSE, 5);
 
-	eventBox = gtk_event_box_new();
-	gtk_event_box_set_above_child(GTK_EVENT_BOX(eventBox), TRUE);
-	gtk_event_box_set_visible_window(GTK_EVENT_BOX(eventBox), FALSE);
+    eventBox = gtk_event_box_new();
+    gtk_event_box_set_above_child(GTK_EVENT_BOX(eventBox), TRUE);
+    gtk_event_box_set_visible_window(GTK_EVENT_BOX(eventBox), FALSE);
 
         // icon
         icon = gtk_image_new();
         gtk_box_pack_start(GTK_BOX(labelBox), icon, FALSE, FALSE, 0);
 
-	// Make the eventbox fill to all left-over space.
-	gtk_box_pack_start(GTK_BOX(labelBox), GTK_WIDGET(eventBox), TRUE, TRUE, 0);
+    // Make the eventbox fill to all left-over space.
+    gtk_box_pack_start(GTK_BOX(labelBox), GTK_WIDGET(eventBox), TRUE, TRUE, 0);
 
-	label = GTK_LABEL(gtk_label_new(text.c_str()));
-	gtk_container_add(GTK_CONTAINER(eventBox), GTK_WIDGET(label));
+    label = GTK_LABEL(gtk_label_new(text.c_str()));
+    gtk_container_add(GTK_CONTAINER(eventBox), GTK_WIDGET(label));
 
-	// Align text to the left (x = 0) and in the vertical center (0.5)
-	gtk_misc_set_alignment(GTK_MISC(label), 0, 0.5);
+    // Align text to the left (x = 0) and in the vertical center (0.5)
+    gtk_misc_set_alignment(GTK_MISC(label), 0, 0.5);
 
-	closeButton = gtk_button_new();
-	gtk_button_set_relief(GTK_BUTTON(closeButton), GTK_RELIEF_NONE);
-	gtk_button_set_focus_on_click(GTK_BUTTON(closeButton), FALSE);
+    closeButton = gtk_button_new();
+    gtk_button_set_relief(GTK_BUTTON(closeButton), GTK_RELIEF_NONE);
+    gtk_button_set_focus_on_click(GTK_BUTTON(closeButton), FALSE);
 
-	// Shrink the padding around the close button
-	GtkRcStyle *rcstyle = gtk_rc_style_new();
-	rcstyle->xthickness = rcstyle->ythickness = 0;
-	gtk_widget_modify_style(closeButton, rcstyle);
-	gtk_rc_style_unref(rcstyle);
+    // Shrink the padding around the close button
+    GtkRcStyle *rcstyle = gtk_rc_style_new();
+    rcstyle->xthickness = rcstyle->ythickness = 0;
+    gtk_widget_modify_style(closeButton, rcstyle);
+    gtk_rc_style_unref(rcstyle);
 
-	// Add the stock icon to the close button
-	GtkWidget *image = gtk_image_new_from_stock(GTK_STOCK_CLOSE, GTK_ICON_SIZE_MENU);
-	gtk_container_add(GTK_CONTAINER(closeButton), image);
-	gtk_box_pack_start(GTK_BOX(labelBox), closeButton, FALSE, FALSE, 0);
+    // Add the stock icon to the close button
+    GtkWidget *image = gtk_image_new_from_stock(GTK_STOCK_CLOSE, GTK_ICON_SIZE_MENU);
+    gtk_container_add(GTK_CONTAINER(closeButton), image);
+    gtk_box_pack_start(GTK_BOX(labelBox), closeButton, FALSE, FALSE, 0);
 
 #if GTK_CHECK_VERSION(2, 12, 0)
-	gtk_widget_set_tooltip_text(closeButton, _("Close tab"));
+    gtk_widget_set_tooltip_text(closeButton, _("Close tab"));
 #else
-	tips = gtk_tooltips_new();
-	g_object_ref_sink(tips);
-	gtk_tooltips_enable(tips);
-	gtk_tooltips_set_tip(tips, closeButton, _("Close tab"), NULL);
+    tips = gtk_tooltips_new();
+    g_object_ref_sink(tips);
+    gtk_tooltips_enable(tips);
+    gtk_tooltips_set_tip(tips, closeButton, _("Close tab"), NULL);
 #endif
-	gtk_widget_show_all(labelBox);
+    gtk_widget_show_all(labelBox);
 
-	tabMenuItem = gtk_radio_menu_item_new_with_label(group, text.c_str());
-	group = gtk_radio_menu_item_get_group(GTK_RADIO_MENU_ITEM(tabMenuItem));
+    tabMenuItem = gtk_radio_menu_item_new_with_label(group, text.c_str());
+    group = gtk_radio_menu_item_get_group(GTK_RADIO_MENU_ITEM(tabMenuItem));
 
-	setLabel_gui(text);
-	setIcon_gui(type);
+    setLabel_gui(text);
+    setIcon_gui(type);
 
-	// Associates entry to the widget for later retrieval in MainWindow::switchPage_gui()
-	g_object_set_data(G_OBJECT(getContainer()), "entry", (gpointer)this);
+    // Associates entry to the widget for later retrieval in MainWindow::switchPage_gui()
+    g_object_set_data(G_OBJECT(getContainer()), "entry", (gpointer)this);
 }
 
 GtkWidget* BookEntry::getContainer()
 {
-	return getWidget("mainBox");
+    return getWidget("mainBox");
 }
 
 void BookEntry::setIcon_gui(const EntryType type)
 {
-	string stock;
-	switch (type)
-	{
-		case Entry::FAVORITE_HUBS : stock = WGETS("icon-favorite-hubs"); break;
-		case Entry::FAVORITE_USERS : stock = WGETS("icon-favorite-users"); break;
-		case Entry::PUBLIC_HUBS : stock = WGETS("icon-public-hubs"); break;
-		case Entry::DOWNLOAD_QUEUE : stock = WGETS("icon-queue"); break;
-		case Entry::SEARCH : stock = WGETS("icon-search"); break;
-		case Entry::SEARCH_ADL : stock = WGETS("icon-search-adl"); break;
-		case Entry::SEARCH_SPY : stock = WGETS("icon-search-spy"); break;
-		case Entry::FINISHED_DOWNLOADS : stock = WGETS("icon-finished-downloads"); break;
-		case Entry::FINISHED_UPLOADS : stock = WGETS("icon-finished-uploads"); break;
-		case Entry::PRIVATE_MESSAGE : stock = WGETS("icon-pm-online"); break;
-		case Entry::HUB : stock = WGETS("icon-hub-offline"); break;
-		case Entry::SHARE_BROWSER : stock = WGETS("icon-directory"); break;
-		default: ;
-	}
-	gtk_image_set_from_stock(GTK_IMAGE(icon), stock.c_str(), GTK_ICON_SIZE_BUTTON);
+    string stock;
+    switch (type)
+    {
+        case Entry::FAVORITE_HUBS : stock = WGETS("icon-favorite-hubs"); break;
+        case Entry::FAVORITE_USERS : stock = WGETS("icon-favorite-users"); break;
+        case Entry::PUBLIC_HUBS : stock = WGETS("icon-public-hubs"); break;
+        case Entry::DOWNLOAD_QUEUE : stock = WGETS("icon-queue"); break;
+        case Entry::SEARCH : stock = WGETS("icon-search");printf("%s \n",stock.c_str()); break;
+        case Entry::SEARCH_ADL : stock = WGETS("icon-search-adl"); break;
+        case Entry::SEARCH_SPY : stock = WGETS("icon-search-spy"); break;
+        case Entry::FINISHED_DOWNLOADS : stock = WGETS("icon-finished-downloads"); break;
+        case Entry::FINISHED_UPLOADS : stock = WGETS("icon-finished-uploads"); break;
+        case Entry::PRIVATE_MESSAGE : stock = WGETS("icon-pm-online"); break;
+        case Entry::HUB : stock = WGETS("icon-hub-offline"); break;
+        case Entry::SHARE_BROWSER : stock = WGETS("icon-directory"); break;
+        default: ;
+    }
+    gtk_image_set_from_icon_name(GTK_IMAGE(icon), stock.c_str(), GTK_ICON_SIZE_BUTTON);
 }
 
 void BookEntry::setIcon_gui(const std::string stock)
 {
-	gtk_image_set_from_stock(GTK_IMAGE(icon), stock.c_str(), GTK_ICON_SIZE_BUTTON);
+    gtk_image_set_from_icon_name(GTK_IMAGE(icon), stock.c_str(), GTK_ICON_SIZE_BUTTON);
 }
 
 void BookEntry::setLabel_gui(string text)
 {
-	// Update the tab menu item label
-	GtkWidget *child = gtk_bin_get_child(GTK_BIN(tabMenuItem));
-	if (child && GTK_IS_LABEL(child))
-		gtk_label_set_text(GTK_LABEL(child), text.c_str());
+    // Update the tab menu item label
+    GtkWidget *child = gtk_bin_get_child(GTK_BIN(tabMenuItem));
+    if (child && GTK_IS_LABEL(child))
+        gtk_label_set_text(GTK_LABEL(child), text.c_str());
 
-	// Update the notebook tab label
+    // Update the notebook tab label
 #if GTK_CHECK_VERSION(2, 12, 0)
-	gtk_widget_set_tooltip_text(eventBox, text.c_str());
+    gtk_widget_set_tooltip_text(eventBox, text.c_str());
 #else
-	gtk_tooltips_set_tip(tips, eventBox, text.c_str(), text.c_str());
+    gtk_tooltips_set_tip(tips, eventBox, text.c_str(), text.c_str());
 #endif
-	glong len = g_utf8_strlen(text.c_str(), -1);
+    glong len = g_utf8_strlen(text.c_str(), -1);
 
-	// Truncate the label text
-	if (len > labelSize)
-	{
-		gchar truncatedText[text.size()];
-		const string clipText = "...";
-		len = labelSize - g_utf8_strlen(clipText.c_str(), -1);
-		g_utf8_strncpy(truncatedText, text.c_str(), len);
-		truncatedLabelText = truncatedText + clipText;
-	}
-	else
-	{
-		truncatedLabelText = text;
-	}
+    // Truncate the label text
+    if (len > labelSize)
+    {
+        gchar truncatedText[text.size()];
+        const string clipText = "...";
+        len = labelSize - g_utf8_strlen(clipText.c_str(), -1);
+        g_utf8_strncpy(truncatedText, text.c_str(), len);
+        truncatedLabelText = truncatedText + clipText;
+    }
+    else
+    {
+        truncatedLabelText = text;
+    }
 
-	labelText = text;
-	updateLabel_gui();
+    labelText = text;
+    updateLabel_gui();
 
-	// Update the main window title if the current tab is selected.
-	if (isActive_gui())
-		WulforManager::get()->getMainWindow()->setTitle(getLabelText());
+    // Update the main window title if the current tab is selected.
+    if (isActive_gui())
+        WulforManager::get()->getMainWindow()->setTitle(getLabelText());
 }
 
 void BookEntry::setBold_gui()
 {
-	if (!bold && !isActive_gui())
-	{
-		bold = TRUE;
-		updateLabel_gui();
-	}
+    if (!bold && !isActive_gui())
+    {
+        bold = TRUE;
+        updateLabel_gui();
+    }
 }
 
 void BookEntry::setUrgent_gui()
 {
-	if (!isActive_gui())
-	{
-		MainWindow *mw = WulforManager::get()->getMainWindow();
+    if (!isActive_gui())
+    {
+        MainWindow *mw = WulforManager::get()->getMainWindow();
 
-		if (!urgent)
-		{
-			bold = TRUE;
-			urgent = TRUE;
-			updateLabel_gui();
-		}
+        if (!urgent)
+        {
+            bold = TRUE;
+            urgent = TRUE;
+            updateLabel_gui();
+        }
 
-		if (!mw->isActive_gui())
-			mw->setUrgent_gui();
-	}
+        if (!mw->isActive_gui())
+            mw->setUrgent_gui();
+    }
 }
 
 void BookEntry::setActive_gui()
 {
-	if (bold || urgent)
-	{
-		bold = FALSE;
-		urgent = FALSE;
-		updateLabel_gui();
-	}
+    if (bold || urgent)
+    {
+        bold = FALSE;
+        urgent = FALSE;
+        updateLabel_gui();
+    }
 }
 
 bool BookEntry::isActive_gui()
 {
-	MainWindow *mw = WulforManager::get()->getMainWindow();
+    MainWindow *mw = WulforManager::get()->getMainWindow();
 
-	return mw->isActive_gui() && mw->currentPage_gui() == getContainer();
+    return mw->isActive_gui() && mw->currentPage_gui() == getContainer();
 }
 
 void BookEntry::updateLabel_gui()
 {
-	const char *format = "%s";
+    const char *format = "%s";
 
-	if (urgent)
-		format = "<i><b>%s</b></i>";
-	else if (bold)
-		format = "<b>%s</b>";
+    if (urgent)
+        format = "<i><b>%s</b></i>";
+    else if (bold)
+        format = "<b>%s</b>";
 
-	char *markup = g_markup_printf_escaped(format, truncatedLabelText.c_str());
-	gtk_label_set_markup(label, markup);
-	g_free(markup);
+    char *markup = g_markup_printf_escaped(format, truncatedLabelText.c_str());
+    gtk_label_set_markup(label, markup);
+    g_free(markup);
 }
 
 const string& BookEntry::getLabelText()
 {
-	return labelText;
+    return labelText;
 }
 
