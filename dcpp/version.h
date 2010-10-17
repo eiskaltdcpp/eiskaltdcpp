007704eff5094c88ecb1c1535caff90f975ae149
Eugene Petrov <dhamp@ya.ru>
2010-10-17 23:33:13 +0400

    откат версии

--- dcpp/version.h
+++ dcpp/version.h
@@ -19,7 +19,7 @@
 #define APPNAME "EiskaltDC++"
 #define VERSIONSTRING "2.1.0"
 #define VERSIONFLOAT 2.1
-#define DCVERSIONSTRING "0.777"
+#define DCVERSIONSTRING "0.770"
 
 namespace dcpp {
 extern const char * const fullVersionString;
