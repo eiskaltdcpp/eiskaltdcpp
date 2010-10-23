b7a853d35ebb09f384fa885aa5610f70265b974a
Boris Pek <tehnick-8@yandex.ru>
2010-10-23 19:33:40 +0300

    fix ver

--- Version.h
+++ Version.h
@@ -11,7 +11,7 @@
 #define VERSION_H
 
 const char * const EISKALTDCPP_VERSION         = "2.1";
-const char * const EISKALTDCPP_VERSION_SFX     = "GIT";
+const char * const EISKALTDCPP_VERSION_SFX     = "2.1.x";
 const char * const EISKALTDCPP_WND_TITLE       = "EiskaltDC++";
 
 #endif // VERSION_H
