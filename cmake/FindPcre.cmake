6742129453ed158daf0ddaaf0a17fb1f7ef26ee4
Eugene Petrov <dhamp@ya.ru>
2010-11-21 21:48:05 +0300

    fix findpcre.cmake

--- cmake/FindPcre.cmake
+++ cmake/FindPcre.cmake
@@ -1,59 +1,40 @@
 if (PCRE_INCLUDE_DIR AND PCRE_LIBRARY)
   # Already in cache, be silent
-  set(PCRE_FIND_QUIETLY TRUE)
-endif (PCRE_INCLUDE_DIR AND PCRE_LIBRARY)
+  set(PCREPP_FIND_QUIETLY TRUE)
+endif ()
 
-#find_path(PCRE_INCLUDE_DIR pcre.h
-   #PATH_SUFFIXES pcre)
-#find_library(PCRE_LIBRARY pcre)
-#find_path(PCRE++_INCLUDE_DIR pcre++.h
-   #PATH_SUFFIXES pcre++)
-#find_library(PCRE++_LIBRARY pcre++)
 find_program(PCRE_CONFIG pcre-config)
-#find_program(PCRE++_CONFIG pcre++-config)
-pkg_check_modules(PCRE pcrecpp)
-execute_process (COMMAND ${PCRE_CONFIG} --libs-cpp
-                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-                OUTPUT_VARIABLE PCRE_LIBRARY
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process (COMMAND ${PCRE_CONFIG} --cflags
-                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-                OUTPUT_VARIABLE PCRE_INCLUDE_DIR
-                OUTPUT_STRIP_TRAILING_WHITESPACE)
+#`pcre-config --cflags`
+#`pcre-config --libs-cpp`
+#pkg_check_modules(PCRE libpcre libpcrecpp libpcreposix)
 
-find_path(RE_INCLUDE_DIR pcre.h
-   PATH_SUFFIXES pcre)
-set (PCRE_INCLUDE_DIR ${RE_INCLUDE_DIR})
-#find_library(PCRE_PCRE_LIBRARY pcre )
-##execute_process (COMMAND ${PCRE++_CONFIG} --libs
-                ##WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-                ##OUTPUT_VARIABLE PCRE++_LIBRARY)
-##execute_process (COMMAND ${PCRE_CONFIG} --cflags
-                ##WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-                ##OUTPUT_VARIABLE PCRE++_INCLUDE_DIR)
-#message(STATUS "${PCRE_LIBRARIES}")
-#message(STATUS "${PCRE_LIBRARY_DIRS}")
-#message(STATUS "${PCRE_LDFLAGS}")
-#message(STATUS "${PCRE_LDFLAGS_OTHER}")
-#message(STATUS "${PCRE_INCLUDE_DIRS}")
-#message(STATUS "${PCRE_CFLAGS}")
-#message(STATUS "${PCRE_CFLAGS_OTHER}")
-#message(STATUS "${PCRE_LIBRARY}")
-#message(STATUS "${PCRE_INCLUDE_DIR}")
-#message(STATUS "${RE_INCLUDE_DIR}")
+pkg_check_modules(PCRE libpcrecpp)
 
-#set(PCRE_INCLUDE_DIR ${PCRE_INCLUDE_DIRS})
-#set (PCRE_LIBRARY ${PCRE_LDFLAGS})
+set (PCRE_INCLUDE_DIR ${PCRE_INCLUDE_DIRS})
+set (PCRE_LIBRARY ${PCRE_LDFLAGS})
+
+if (NOT PCRE_LIBRARY)
+    execute_process (COMMAND ${PCRE_CONFIG} --libs-cpp
+                    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                    OUTPUT_VARIABLE RE_LIBRARY
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+                    set (PCRE_LIBRARY ${RE_LIBRARY})
+endif (PCRE_LIBRARY)
+
+if (NOT PCRE_INCLUDE_DIR)
+    find_path(RE_INCLUDE_DIR pcrecpp.h)
+    set (PCRE_INCLUDE_DIR ${RE_INCLUDE_DIR})
+endif()
 
 if (PCRE_INCLUDE_DIR AND PCRE_LIBRARY)
     set (PCRE_FOUND TRUE)
 endif (PCRE_INCLUDE_DIR AND PCRE_LIBRARY)
 
 if (PCRE_FOUND)
-  if (NOT PCRE_FIND_QUIETLY)
+  if (NOT PCREPP_FIND_QUIETLY)
     message (STATUS "Found the pcre(cpp) libraries at ${PCRE_LIBRARY}")
     message (STATUS "Found the pcre(cpp) headers at ${PCRE_INCLUDE_DIR}")
-  endif (NOT PCRE_FIND_QUIETLY)
+  endif ()
 else ()
     message (STATUS "Could not find pcre(cpp)")
 endif ()
@@ -62,5 +43,3 @@ MARK_AS_ADVANCED(
   PCRE_INCLUDE_DIR
   PCRE_LIBRARY
 )
-#`pcre-config --cflags`
-#`pcre-config --libs-cpp`
