1ee304ec217fdff5d21061f13dbe89d0ec6d195f
Andrey Karlov <dein.negativ@gmail.com>
2010-12-28 16:40:55 +0300

    DC++ Core: 	* Fix build on non-POSIX systems.

--- dcpp/Socket.cpp
+++ dcpp/Socket.cpp
@@ -28,6 +28,13 @@
 #define MSG_NOSIGNAL 0
 #endif
 
+#ifndef IP_TOS
+#define        IP_TOS          1
+#endif
+#ifndef IPTOS_TOS
+#define IPTOS_TOS(a) ((a) & 0x1E)
+#endif
+
 namespace dcpp {
 
 string Socket::udpServer;
