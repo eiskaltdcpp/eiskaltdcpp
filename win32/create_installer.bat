d263244503628ca631c110d55d8ffc8667d2a299
Pavel Vatagin <pavelvat@gmail.com>
2011-04-17 15:59:51 +0400

    win32, update bat files and NSIS script

--- win32/create_installer.bat
+++ win32/create_installer.bat
@@ -14,6 +14,8 @@ copy /Y "%SOURCESDIR%\icons\eiskaltdcpp.ico"                          %DIRINSTAL
 copy /Y "%SOURCESDIR%\icons\icon_164x314.bmp"                         %DIRINSTALLER%
 copy /Y "%SOURCESDIR%\win32\dcppboot.xml"                             %DIRINSTALLER%
 copy /Y "%SOURCESDIR%\LICENSE"                                        %DIRINSTALLER%
+echo [Paths] > "%DIRINSTALLER%\qt.conf"
+echo Plugins = ./plugins >> "%DIRINSTALLER%\qt.conf"
 
 copy /Y "%QTDIR%\bin\QtCore4.dll"                                     %DIRINSTALLER%
 copy /Y "%QTDIR%\bin\QtGui4.dll"                                      %DIRINSTALLER%
@@ -45,9 +47,17 @@ copy /Y "%MINGW%\bin\iconv.dll"                                       %DIRINSTAL
 copy /Y "%MINGW%\bin\mgwz.dll"                                        %DIRINSTALLER%
 copy /Y "%MINGW%\bin\mgwbz2-1.dll"                                    %DIRINSTALLER%
 copy /Y "%MINGW%\bin\lua51.dll"                                       %DIRINSTALLER%
+copy /Y "%MINGW%\bin\libidn-11.dll"                                   %DIRINSTALLER%
 copy /Y "%MINGW%\bin\libaspell-15.dll"                                %DIRINSTALLER%
+
+mkdir "%DIRINSTALLER%\aspell\data\"
+mkdir "%DIRINSTALLER%\aspell\dict\"
+copy /Y "%MINGW%\lib\aspell-0.60\*"                                   %DIRINSTALLER%\aspell\data\
 
 copy /Y "%SystemRoot%\System32\ssleay32.dll"                          %DIRINSTALLER%
 copy /Y "%SystemRoot%\System32\libeay32.dll"                          %DIRINSTALLER%
+
+mkdir "%DIRINSTALLER%\plugins\sqldrivers\"
+copy /Y "%QTDIR%\plugins\sqldrivers\qsqlite4.dll"                     %DIRINSTALLER%\plugins\sqldrivers\
 
 "%ProgramFiles%\NSIS\makensis.exe" "%BUILDDIR%\EiskaltDC++.nsi"
