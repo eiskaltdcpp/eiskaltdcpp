e38e77962ab78b36bca8032d9ca27b9f2164d645
Dmitry Kolosov <onyx@z-up.ru>
2011-09-10 19:20:39 +0400

    Initial eiskaltdcpp-cli commit

---
+++ eiskaltdcpp-cli/config.pl
@@ -0,0 +1,8 @@
+$config{prompt}="not connected# ";
+$config{separator}=";";
+$config{eiskaltURL} = "http://localhost:8080/eiskaltdcpp";
+$config{version}=0.1;
+$config{hist_file}="~/.eiskaltdcpp_cli.hist";
+$config{hist_max}=500;
+
+1;
