6497c49b72eb68a2a65a44e32d79c90585d70f1b
Dmitry Kolosov <onyx@z-up.ru>
2011-09-16 10:35:48 +0400

    * +README * +TODO * cli.pl updated, tested (but search methods due to development) * config.pl updated

--- eiskaltdcpp-cli/config.pl
+++ eiskaltdcpp-cli/config.pl
@@ -1,6 +1,7 @@
-$config{prompt}="not connected# ";
+$config{prompt}="# ";
 $config{separator}=";";
-$config{eiskaltURL} = "http://localhost:8080/eiskaltdcpp";
+$config{eiskaltHostPort}="localhost:8080";
+$config{eiskaltURL} = "http://$config{eiskaltHostPort}/eiskaltdcpp";
 $config{version}=0.1;
 $config{hist_file}="~/.eiskaltdcpp_cli.hist";
 $config{hist_max}=500;
