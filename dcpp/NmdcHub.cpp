1a3ad1b152055ed2bd9186fc93b2df06e2c749e1
Boris Pek <tehnick-8@yandex.ru>
2012-04-10 21:46:42 +0300

    DC++ Core: * removed hublist link on adcportal.com (URL is not valid now). + doc up

--- dcpp/NmdcHub.cpp
+++ dcpp/NmdcHub.cpp
@@ -1067,7 +1067,6 @@ void NmdcHub::on(Minute, uint64_t aTick) noexcept {
         protectedIPs.push_back("openhublist.org");
         protectedIPs.push_back("dchublist.com");
         protectedIPs.push_back("hublista.hu");
-        protectedIPs.push_back("adcportal.com");
         for(StringIter i = protectedIPs.begin(); i != protectedIPs.end();) {
             *i = Socket::resolve(*i);
             if(Util::isPrivateIp(*i))
