6b3a72680bd427401ad0339e3792cb9b998a455a
Eugene Petrov <dhamp@ya.ru>
2012-06-02 09:16:16 +0400

    Daemon: fix build with clang

--- dcpp/noexcept.h
+++ dcpp/noexcept.h
@@ -20,7 +20,17 @@
 
 // for compilers that don't support noexcept, use an exception specifier
 
-#ifdef __GNUC__
+#if defined(__clang__)
+
+#if __clang_major__ < 3
+
+#ifndef noexcept
+#define noexcept throw()
+#endif
+
+#endif
+
+#elif defined(__GNUC__)
 #if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 6) // GCC 4.6 is the first GCC to implement noexcept.
 
 #ifndef noexcept
