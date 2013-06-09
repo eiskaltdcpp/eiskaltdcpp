857c61e93d7679a468512d7ccf0cdee1ec67ee4d
Boris Pek <tehnick-8@yandex.ru>
2013-06-09 20:35:41 +0300

    Gtk UI: add progressbar into search widget (step 3): * properly initialize variables * improve search interval (but this is still not final variant)

--- eiskaltdcpp-gtk/src/search.cc
+++ eiskaltdcpp-gtk/src/search.cc
@@ -43,11 +43,15 @@ Search::Search():
 #if GTK_CHECK_VERSION(3,0,0)
     gtk_progress_bar_set_show_text(GTK_PROGRESS_BAR(getWidget("progressbar1")), TRUE);
 #else
-    gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(getWidget("progressbar1")), 0.0);
     gtk_statusbar_set_has_resize_grip (GTK_STATUSBAR(getWidget("statusbar2")),FALSE);
     gtk_statusbar_set_has_resize_grip (GTK_STATUSBAR(getWidget("statusbar3")),FALSE);
 #endif
 
+    // Initialize variables for search progressbar
+    waitingResults = false;
+    searchStartTime = 0;
+    searchEndTime = 1;
+    setProgress_gui("progressbar1", "", 0.0);
 
     // Initialize the search entries combo box
     if (searchEntriesModel == NULL)
@@ -552,16 +556,18 @@ void Search::search_gui()
     setStatus_gui("statusbar3", _("0 filtered"));
     setLabel_gui(text);
 
-    dcdebug(_("Sent ADC extensions : %s\n"), Util::toString(";", exts).c_str());
-
     searchStartTime = GET_TICK();
     target = text;
 
+    dcdebug(_("Sent ADC extensions : %s\n"), Util::toString(";", exts).c_str());
+    uint64_t delayBeforeSearch = SearchManager::getInstance()->search(clients, text, llsize, (SearchManager::TypeModes)ftype, mode, "manual", exts, (void*)this);
+    uint64_t searchInterval = SETTING(MINIMUM_SEARCH_INTERVAL)*1000; // temporary variant; we should use the maximum value from selected hubs
+
+    searchEndTime = searchStartTime + delayBeforeSearch + searchInterval;
+    waitingResults = true;
+
     if (WGETB("clearsearch")) // Only clear if the search was sent.
         gtk_entry_set_text(GTK_ENTRY(searchEntry), "");
-
-    searchEndTime = searchStartTime + SearchManager::getInstance()->search(clients, text, llsize, (SearchManager::TypeModes)ftype, mode, "manual", exts, (void*)this) + 5000;
-    waitingResults = true;
 
     if (gtk_widget_get_visible(getWidget("sidePanel")) && !gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(getWidget("checkDontHideSideOnSearch"))))
         gtk_widget_hide(getWidget("sidePanel"));
