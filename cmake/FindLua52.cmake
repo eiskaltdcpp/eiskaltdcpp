66d9672396860cb27fcf275eac986d29385f34e6
Eugene Petrov <dhamp@ya.ru>
2013-06-24 18:24:13 +0400

    cmake: fix findlua.cmake

--- cmake/FindLua52.cmake
+++ cmake/FindLua52.cmake
@@ -80,7 +80,7 @@ endif ()
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LUA_FOUND to TRUE if
 # all listed variables are TRUE
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Lua51
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Lua52
                                   REQUIRED_VARS LUA_LIBRARIES LUA_INCLUDE_DIR
                                   VERSION_VAR LUA_VERSION_STRING)
 
