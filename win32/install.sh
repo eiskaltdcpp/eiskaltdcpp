aefec2562aee4feb49eaf12008a2a008199c4ae1
Pavel Vatagin <pavelvat@gmail.com>
2013-07-13 00:31:03 +0400

    win32 up

--- win32/install.sh
+++ win32/install.sh
@@ -8,6 +8,7 @@ strip "$INSTALL_DIR/eiskaltdcpp-qt.exe"
 strip "$INSTALL_DIR/eiskaltdcpp-daemon.exe"
 
 cp $SOURCES_DIR/win32/dcppboot.xml                        "$INSTALL_DIR"
+cp $SOURCES_DIR/eiskaltdcpp-cli/cli-jsonrpc-config.pl     "$INSTALL_DIR"
 echo [Paths] > "$INSTALL_DIR/qt.conf"
 echo Plugins = ./plugins >> "$INSTALL_DIR/qt.conf"
 
