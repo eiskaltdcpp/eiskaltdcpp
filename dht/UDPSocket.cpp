51238f60efed4267f0cfc5051e46f5508793786e
Eugene Petrov <dhamp@ya.ru>
2014-04-26 00:30:38 +0400

    enable listen dht on ipv6 != dht work with ipv6 (need rewrite partial)

--- dht/UDPSocket.cpp
+++ dht/UDPSocket.cpp
@@ -85,6 +85,7 @@ namespace dht
         try
         {
             socket.reset(new Socket(Socket::TYPE_UDP));
+            socket->setV4only(false);
             socket->setLocalIp4(SETTING(BIND_IFACE)? socket->getIfaceI4(SETTING(BIND_IFACE_NAME)).c_str() : SETTING(BIND_ADDRESS));
             socket->setLocalIp6(SETTING(BIND_IFACE6)? socket->getIfaceI6(SETTING(BIND_IFACE_NAME6)).c_str() : SETTING(BIND_ADDRESS6));
             port = socket->listen(Util::toString(SETTING(DHT_PORT)));
