9645e5350d1b8ea2145995a5f52ed928cb60e6f5
Eugene Petrov <dhamp@ya.ru>
2014-05-10 02:52:56 +0400

    CLI:       update TODO Daemon:       del method methods.list - use method system.describe       add methos list.downloaddir

--- eiskaltdcpp-daemon/jsonrpcmethods.h
+++ eiskaltdcpp-daemon/jsonrpcmethods.h
@@ -56,4 +56,5 @@ class JsonRpcMethods
     bool CloseAllFileLists(const Json::Value& root, Json::Value& response);
     bool ShowOpenedLists(const Json::Value& root, Json::Value& response);
     bool LsDirInList(const Json::Value& root, Json::Value& response);
+    bool DownloadDirFromList(const Json::Value &root, Json::Value &response);
 };
