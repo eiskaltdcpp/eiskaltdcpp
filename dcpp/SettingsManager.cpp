9973a6f41fdd117d1109fcfb4e5a27a8a162ce74
Eugene Petrov <dhamp@ya.ru>
2014-05-22 02:22:35 +0400

    add TLS options from core in gui options + add flags

--- dcpp/SettingsManager.cpp
+++ dcpp/SettingsManager.cpp
@@ -96,7 +96,7 @@ const string SettingsManager::settingTags[] =
     "IpFilter", "TextColor", "UseLua", "AllowNatt", "IpTOSValue", "SegmentSize",
     "BindIface", "MinimumSearchInterval", "EnableDynDNS", "AllowUploadOverMultiHubs",
     "UseADLOnlyOnOwnList", "AllowSimUploads", "CheckTargetsPathsOnStart", "NmdcDebug",
-    "ShareSkipZeroByte", "LogSpy",
+    "ShareSkipZeroByte", "RequireTLS", "LogSpy",
     "SENTRY",
     // Int64
     "TotalUpload", "TotalDownload",
