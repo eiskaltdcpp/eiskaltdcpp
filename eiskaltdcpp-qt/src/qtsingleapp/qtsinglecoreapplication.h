aefadbe889d3dec067f422d126c3e9864bed1276
Eugene Petrov <dhamp@ya.ru>
2015-04-19 22:59:48 +0300

    Cmake up + fix build

--- eiskaltdcpp-qt/src/qtsingleapp/qtsinglecoreapplication.h
+++ eiskaltdcpp-qt/src/qtsingleapp/qtsinglecoreapplication.h
@@ -7,6 +7,7 @@
 *                                                                         *
 ***************************************************************************/
 
+#pragma once
 
 #include <QApplication>
 #include <QSharedMemory>
