cbf760e117cf435281d73c67a2ba6bc991b7b511
Pavel Vatagin <pavelvat@gmail.com>
2016-11-18 01:32:11 +0300

    Tests: update scripts (reduce number of targets)

--- tests/travis/build-and-test.sh
+++ tests/travis/build-and-test.sh
@@ -6,23 +6,19 @@
 
 set -x
 
-if [ "${OS}" != "mingw" ]; then
+mkdir -p builddir
+cd builddir
+
+if [ "${OS}" != "Windows" ]; then
     export CXXFLAGS="$(dpkg-buildflags --get CXXFLAGS) $(dpkg-buildflags --get CPPFLAGS)"
     export LDFLAGS="$(dpkg-buildflags --get LDFLAGS) -Wl,--as-needed"
     if [ "${USE_QT}" = "qt5" ]; then
         CXXFLAGS="${CXXFLAGS} -fPIC"
     fi
-fi
 
-mkdir -p builddir
-cd builddir
-
-CMAKEOPTS="..
-           -DCMAKE_INSTALL_PREFIX=/usr
-           -DCMAKE_BUILD_TYPE=RelWithDebInfo"
-
-if [ "${CONFIG}" = "full" ]; then
-    CMAKEOPTS="${CMAKEOPTS}
+    CMAKEOPTS="..
+               -DCMAKE_INSTALL_PREFIX=/usr
+               -DCMAKE_BUILD_TYPE=Release
                -DUSE_MINIUPNP=ON
                -DLOCAL_MINIUPNP=OFF
                -DWITH_DHT=ON
@@ -32,99 +28,58 @@ if [ "${CONFIG}" = "full" ]; then
                -DWITH_DEV_FILES=ON
                -DPERL_REGEX=ON
                -DWITH_SOUNDS=ON"
-else
-    CMAKEOPTS="${CMAKEOPTS}
-               -Dlinguas=''
-               -DUSE_MINIUPNP=OFF
-               -DWITH_DHT=OFF
-               -DUSE_IDNA=OFF
-               -DLUA_SCRIPT=OFF
-               -DWITH_LUASCRIPTS=OFF
-               -DWITH_DEV_FILES=OFF
-               -DPERL_REGEX=OFF
-               -DWITH_SOUNDS=OFF"
-fi
 
+    if [ ! -z "${USE_QT}" ]; then
+        if [ "${USE_QT}" = "qt4" ]; then
+            CMAKEOPTS="${CMAKEOPTS}
+                       -DUSE_QT=ON
+                       -DUSE_QT5=OFF"
+        elif [ "${USE_QT}" = "qt5" ]; then
+            CMAKEOPTS="${CMAKEOPTS}
+                       -DUSE_QT=OFF
+                       -DUSE_QT5=ON"
+        fi
+            CMAKEOPTS="${CMAKEOPTS}
+                       -DDBUS_NOTIFY=ON
+                       -DWITH_EMOTICONS=ON
+                       -DWITH_EXAMPLES=ON
+                       -DUSE_JS=ON
+                       -DUSE_QT_QML=ON
+                       -DUSE_ASPELL=ON
+                       -DUSE_QT_SQLITE=ON"
+    fi
 
-if [ ! -z "${USE_QT}" ]; then
-    if [ "${USE_QT}" = "qt4" ]; then
-        CMAKEOPTS="${CMAKEOPTS}
-                   -DUSE_QT=ON
-                   -DUSE_QT5=OFF"
-    elif [ "${USE_QT}" = "qt5" ]; then
+    if [ ! -z "${USE_GTK}" ]; then
         CMAKEOPTS="${CMAKEOPTS}
                    -DUSE_QT=OFF
-                   -DUSE_QT5=ON"
+                   -DUSE_QT5=OFF
+                   -DUSE_LIBGNOME2=OFF
+                   -DCHECK_GTK_DEPRECATED=OFF"
+        if [ "${USE_GTK}" = "gtk2" ]; then
+            CMAKEOPTS="${CMAKEOPTS}
+                       -DUSE_GTK=ON
+                       -DUSE_GTK3=OFF"
+        elif [ "${USE_GTK}" = "gtk3" ]; then
+            CMAKEOPTS="${CMAKEOPTS}
+                       -DUSE_GTK=OFF
+                       -DUSE_GTK3=ON"
+        fi
+            CMAKEOPTS="${CMAKEOPTS}
+                       -DUSE_LIBCANBERRA=ON
+                       -DUSE_LIBNOTIFY=ON"
     fi
-    if [ "${CONFIG}" = "full" ]; then
+
+    if [ ! -z "${USE_DAEMON}" ]; then
         CMAKEOPTS="${CMAKEOPTS}
-                   -DDBUS_NOTIFY=ON
-                   -DWITH_EMOTICONS=ON
-                   -DWITH_EXAMPLES=ON
-                   -DUSE_JS=ON
-                   -DUSE_QT_QML=ON
-                   -DUSE_ASPELL=ON
-                   -DUSE_QT_SQLITE=ON"
-    else
-        CMAKEOPTS="${CMAKEOPTS}
-                   -DDBUS_NOTIFY=OFF
-                   -DWITH_EMOTICONS=OFF
-                   -DWITH_EXAMPLES=OFF
-                   -DUSE_JS=OFF
-                   -DUSE_QT_QML=OFF
-                   -DUSE_ASPELL=OFF
-                   -DUSE_QT_SQLITE=OFF"
+                   -DUSE_QT=OFF
+                   -DUSE_QT5=OFF
+                   -DNO_UI_DAEMON=ON
+                   -DXMLRPC_DAEMON=OFF
+                   -DJSONRPC_DAEMON=ON
+                   -DUSE_CLI_JSONRPC=ON
+                   -DUSE_CLI_XMLRPC=OFF"
     fi
-fi
 
-if [ ! -z "${USE_GTK}" ]; then
-    CMAKEOPTS="${CMAKEOPTS}
-               -DUSE_QT=OFF
-               -DUSE_QT5=OFF
-               -DUSE_LIBGNOME2=OFF
-               -DCHECK_GTK_DEPRECATED=OFF"
-    if [ "${USE_GTK}" = "gtk2" ]; then
-        CMAKEOPTS="${CMAKEOPTS}
-                   -DUSE_GTK=ON
-                   -DUSE_GTK3=OFF"
-    elif [ "${USE_GTK}" = "gtk3" ]; then
-        CMAKEOPTS="${CMAKEOPTS}
-                   -DUSE_GTK=OFF
-                   -DUSE_GTK3=ON"
-    fi
-    if [ "${CONFIG}" = "full" ]; then
-        CMAKEOPTS="${CMAKEOPTS}
-                   -DUSE_LIBCANBERRA=ON
-                   -DUSE_LIBNOTIFY=ON"
-    else
-        CMAKEOPTS="${CMAKEOPTS}
-                   -DUSE_LIBCANBERRA=OFF
-                   -DUSE_LIBNOTIFY=OFF"
-    fi
-fi
-
-if [ ! -z "${USE_DAEMON}" ]; then
-    CMAKEOPTS="${CMAKEOPTS}
-               -DUSE_QT=OFF
-               -DUSE_QT5=OFF
-               -DNO_UI_DAEMON=ON
-               -DXMLRPC_DAEMON=OFF"
-    if [ "${CONFIG}" = "full" ]; then
-        CMAKEOPTS="${CMAKEOPTS}
-                   -DJSONRPC_DAEMON=ON"
-    fi
-fi
-
-if [ ! -z "${USE_CLI}" ]; then
-    CMAKEOPTS="${CMAKEOPTS}
-               -DUSE_QT=OFF
-               -DUSE_QT5=OFF
-               -DUSE_CLI_JSONRPC=ON
-               -DUSE_CLI_XMLRPC=OFF"
-fi
-
-
-if [ "${OS}" != "mingw" ]; then
     cmake ${CMAKEOPTS} \
           -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
           -DCMAKE_SHARED_LINKER_FLAGS="${LDFLAGS}" \
@@ -132,16 +87,13 @@ if [ "${OS}" != "mingw" ]; then
     make VERBOSE=1
     sudo make install
 
-
     du -shc /usr/bin/eiskaltdcpp-*
     du -shc /usr/lib*/libeiskaltdcpp.so*
 
     if [ -z "${USE_DAEMON}" ]; then
         du -shc /usr/share/eiskaltdcpp/*
     fi
-fi
-
-if [ "${OS}" = "mingw" ]; then
+else
     CMAKEOPTS="..
                -DCMAKE_INSTALL_PREFIX=./EiskaltDC++
                -DCMAKE_BUILD_TYPE=Release
--- tests/travis/install-build-depends.sh
+++ tests/travis/install-build-depends.sh
@@ -6,84 +6,62 @@
 
 set -x
 
-# Add debian packages built from MXE packages
-if [ "${OS}" = "mingw" ]; then
-    echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" | sudo tee --append /etc/apt/sources.list.d/mxeapt.list
-    sudo apt-key adv --keyserver x-hkp://keys.gnupg.net --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
-fi
-
-# This  is an ugly hack for partial updating of build environment from
-# Ubuntu 12.04 (Precise Pangolin) to Ubuntu 14.04 (Trusty Tahr):
-if [ "${OS}" != "mingw" ]; then
+if [ "${OS}" != "Windows" ]; then
+    # This  is an ugly hack for partial updating of build environment from
+    # Ubuntu 12.04 (Precise Pangolin) to Ubuntu 14.04 (Trusty Tahr):
     sudo sed -i 's/precise/trusty/g' /etc/apt/sources.list
-fi
 
-sudo apt-get update -qq
-
-# Common build dependencies:
-if [ "${OS}" != "mingw" ]; then
+    sudo apt-get update -qq
     sudo apt-get install -qq cmake \
                              libbz2-dev \
                              libboost-dev \
                              libboost-system-dev \
                              libssl-dev \
                              libattr1-dev \
-                             zlib1g-dev
-fi
-
-if [ "${CONFIG}" = "full" ]; then
-    sudo apt-get install -qq libidn11-dev \
+                             zlib1g-dev \
+                             libidn11-dev \
                              liblua5.1-0-dev \
                              libpcre3-dev \
                              libminiupnpc-dev
-fi
 
-if [ "${USE_QT}" = "qt4" ]; then
-    sudo apt-get install -qq libqt4-dev \
-                             qt4-dev-tools
-    if [ "${CONFIG}" = "full" ]; then
-        sudo apt-get install -qq libaspell-dev
+    if [ "${USE_QT}" = "qt4" ]; then
+        sudo apt-get install -qq libqt4-dev \
+                                 qt4-dev-tools \
+                                 libaspell-dev
+    elif [ "${USE_QT}" = "qt5" ]; then
+        yes | sudo apt-get install -qq qtbase5-dev \
+                                       qttools5-dev \
+                                       qtmultimedia5-dev \
+                                       qtquick1-5-dev \
+                                       qtscript5-dev \
+                                       qt5-default \
+                                       libqt5xmlpatterns5-dev \
+                                       qttools5-dev-tools \
+                                       libaspell-dev
     fi
-elif [ "${USE_QT}" = "qt5" ]; then
-    yes | sudo apt-get --yes --force-yes install -qq qtbase5-dev \
-                             qttools5-dev \
-                             qtmultimedia5-dev \
-                             qtquick1-5-dev \
-                             qtscript5-dev \
-                             qt5-default \
-                             libqt5xmlpatterns5-dev \
-                             qttools5-dev-tools
-    if [ "${CONFIG}" = "full" ]; then
-        sudo apt-get install -qq libaspell-dev
-    fi
-fi
 
-if [ "${USE_GTK}" = "gtk2" ]; then
-    sudo apt-get install -qq libgtk2.0-dev
-    if [ "${CONFIG}" = "full" ]; then
-        sudo apt-get install -qq libnotify-dev \
+    if [ "${USE_GTK}" = "gtk2" ]; then
+        sudo apt-get install -qq libgtk2.0-dev \
+                                 libnotify-dev \
                                  libcanberra-gtk-dev
+    elif [ "${USE_GTK}" = "gtk3" ]; then
+        yes | sudo apt-get install -qq libgtk-3-dev \
+                                       libnotify-dev \
+                                       libcanberra-gtk3-dev
     fi
-elif [ "${USE_GTK}" = "gtk3" ]; then
-    yes | sudo apt-get --yes --force-yes install -qq libgtk-3-dev
-    if [ "${CONFIG}" = "full" ]; then
-        sudo apt-get install -qq libnotify-dev \
-                                 libcanberra-gtk3-dev
+
+    if [ "${USE_DAEMON}" = "ON" ]; then
+        sudo apt-get install -qq libdata-dump-perl \
+                                 libgetopt-long-descriptive-perl \
+                                 libjson-rpc-perl \
+                                 libterm-shellui-perl
     fi
-fi
+else
+    # Add debian packages built from MXE packages
+    echo "deb http://pkg.mxe.cc/repos/apt/debian wheezy main" | sudo tee --append /etc/apt/sources.list.d/mxeapt.list
+    sudo apt-key adv --keyserver x-hkp://keys.gnupg.net --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
 
-if [ "${USE_DAEMON}" = "ON" ]; then
-    echo "There are no additional build dependencies for daemon yet."
-fi
-
-if [ "${USE_CLI}" = "ON" ]; then
-    sudo apt-get install -qq libdata-dump-perl \
-                             libgetopt-long-descriptive-perl \
-                             libjson-rpc-perl \
-                             libterm-shellui-perl
-fi
-
-if [ "${OS}" = "mingw" ]; then
+    sudo apt-get update -qq
     sudo apt-get install -qq mxe-x86-64-w64-mingw32.shared-aspell \
                              mxe-x86-64-w64-mingw32.shared-boost \
                              mxe-x86-64-w64-mingw32.shared-jsoncpp \
