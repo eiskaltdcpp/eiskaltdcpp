655310393e5a5211be7620be7f2cc92c2273150d
Boris Pek <tehnick-8@yandex.ru>
2018-08-22 01:07:40 +0300

    macOS specific changes in build scripts.

--- macos/build-using-homebrew.sh
+++ macos/build-using-homebrew.sh
@@ -10,7 +10,7 @@
 #
 # Notes:
 # brew install wget git htop coreutils
-# brew install cmake gettext libidn openssl jsoncpp miniupnpc aspell qt
+# brew install cmake gettext libidn openssl jsoncpp miniupnpc aspell pcre lua qt
 
 set -e
 
@@ -18,8 +18,8 @@ set -e
 [ -z "${DEPLOYMENT_TARGET}" ] && DEPLOYMENT_TARGET="10.13"
 
 MACOS_SDK="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
-TOOLCHAIN_OPTIONS="-DCMAKE_PREFIX_PATH='${HOMEBREW};${HOMEBREW}/opt/qt;${HOMEBREW}/opt/openssl;${HOMEBREW}/opt/gettext;${HOMEBREW}/opt/libidn2;${MACOS_SDK}' -DCMAKE_MACOSX_RPATH=OFF"
-BUILD_OPTIONS="-DCMAKE_BUILD_TYPE=Release -DUSE_MINIUPNP=ON -DLOCAL_MINIUPNP=ON -DFREE_SPACE_BAR_C=ON -DWITH_SOUNDS=ON -DUSE_ASPELL=ON -DPERL_REGEX=OFF -DWITH_DHT=ON -DLOCAL_BOOST=ON -DUSE_QT=OFF -DUSE_QT5=ON"
+TOOLCHAIN_OPTIONS="-DCMAKE_PREFIX_PATH='${HOMEBREW};${HOMEBREW}/opt/qt;${HOMEBREW}/opt/openssl;${HOMEBREW}/opt/gettext;${HOMEBREW}/opt/libidn2;${MACOS_SDK}'"
+BUILD_OPTIONS="-DCMAKE_BUILD_TYPE=RelWithDebInfo -DUSE_MINIUPNP=ON -DLOCAL_MINIUPNP=ON -DFREE_SPACE_BAR_C=ON -DWITH_SOUNDS=ON -DUSE_ASPELL=ON -DPERL_REGEX=ON -DLUA_SCRIPT=ON -DWITH_DHT=ON -DLOCAL_BOOST=ON -DUSE_QT=OFF -DUSE_QT5=ON"
 ARCH="-DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET='${DEPLOYMENT_TARGET}' -DCMAKE_OSX_SYSROOT='${MACOS_SDK}'"
 COMPILER="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
 
@@ -31,7 +31,7 @@ mkdir -p "${MAIN_DIR}/builddir"
 cd "${MAIN_DIR}/builddir"
 
 cmake .. ${TOOLCHAIN_OPTIONS} ${BUILD_OPTIONS} ${ARCH} ${COMPILER} "$@"
-make -j4
+cmake --build . --target all --parallel 4
 
 cpack -G DragNDrop
 cp -a EiskaltDC++*.dmg "${MAIN_DIR}/../"
