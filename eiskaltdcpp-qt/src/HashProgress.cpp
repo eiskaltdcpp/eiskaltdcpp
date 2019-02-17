8db49f0d182ce6eae4a3f6048053fdc5ad97db7b
Boris Pek <tehnick-8@yandex.ru>
2019-02-17 14:15:17 +0300

    [Qt] More changes related to USE_PROGRESS_BARS.

--- eiskaltdcpp-qt/src/HashProgress.cpp
+++ eiskaltdcpp-qt/src/HashProgress.cpp
@@ -52,6 +52,13 @@ HashProgress::HashProgress(QWidget *parent):
     setWindowModality(Qt::ApplicationModal);
     setAttribute( Qt::WA_QuitOnClose, false ); // Very important, wo this line app exits then hide
 
+#if defined(USE_PROGRESS_BARS)
+    progressIndicator->hide();
+    this->adjustSize();
+#else
+    progress->setFormat(QString());
+#endif
+
     HashManager::getInstance()->setPriority(Thread::NORMAL);
 
     timer = new QTimer();
@@ -111,7 +118,7 @@ void HashProgress::timerTick(){
         return;;
     }
 
-    double diff = tick - startTime;
+    const double diff = tick - startTime;
     bool paused = HashManager::getInstance()->isHashingPaused();
 
     QString eta;
@@ -123,11 +130,16 @@ void HashProgress::timerTick(){
         progress->setValue((10000*(startBytes - bytes))/startBytes);
     }
 
-    if( diff == 0. || files == 0 || bytes == 0 || paused) {
+    if(static_cast<uint64_t>(diff) == 0 || files == 0 || bytes == 0 || paused) {
         status->setText(QString(tr("-.-- files/h, %1 files left")).arg((uint32_t)files));
         speed->setText(tr("-.-- B/s, %1 left").arg(WulforUtil::formatBytes(bytes)));
         eta = tr("-:--:--");
-        progress->setToolTip(QString());
+
+#if !defined(USE_PROGRESS_BARS)
+        progressIndicator->setText(tr("%1% %2 left")
+                                   .arg(QString::number(0))
+                                   .arg(eta));
+#endif
     }
     else {
         double filestat = (((double)(startFiles - files)) * 60 * 60 * 1000) / diff;
@@ -147,13 +159,14 @@ void HashProgress::timerTick(){
             eta = _q(Text::toT(Util::formatSeconds((int64_t)(ss))));
         }
 
-#if !defined(USE_PROGRESS_BARS)
-    progress->setToolTip(tr("%1% %2 left")
-                         .arg(QString::number((100*(startBytes - bytes))/startBytes))
-                         .arg(eta));
+#if defined(USE_PROGRESS_BARS)
+        progress->setFormat(tr("%p% %1 left").arg(eta));
+#else
+        progressIndicator->setText(tr("%1% %2 left")
+                                   .arg(QString::number((100*(startBytes - bytes))/startBytes))
+                                   .arg(eta));
 #endif
     }
-    progress->setFormat(tr("%p% %1 left").arg(eta));
 
     if(!files) {
         //progress->setValue(10000); // generates anoying blinking 0 -> 100%
