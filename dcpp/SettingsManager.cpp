aa207f0a5ab3e31d8bb6b5514701c96b7843b106
Boris Pek <tehnick-8@yandex.ru>
2019-03-14 02:10:46 +0300

    [Core] Enable DHT by default.
    (Affects only new users.) In case you need you may
    change this option at any time in Preferences dialog:
    Connection --> Advanced --> Enable DHT.

--- dcpp/SettingsManager.cpp
+++ dcpp/SettingsManager.cpp
@@ -277,7 +277,7 @@ SettingsManager::SettingsManager()
     setDefault(HASH_BUFFER_PRIVATE, true);
     setDefault(RECONNECT_DELAY, 15);
     setDefault(DHT_PORT, 6250);
-    setDefault(USE_DHT, false);
+    setDefault(USE_DHT, true);
     setDefault(SEARCH_PASSIVE, false);
     setDefault(AUTO_DETECT_CONNECTION, false);
     setDefault(MAX_UPLOAD_SPEED_MAIN, 0);
