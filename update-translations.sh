f7dcf151d27c80b2f6ea6a2f2bf2267540d5e5d7
Boris Pek <tehnick-8@yandex.ru>
2019-04-07 00:36:10 +0300

    Improve update-translations script.
    + Update Transifex config.

--- update-translations.sh
+++ update-translations.sh
@@ -1,43 +1,47 @@
-#! /bin/bash
+#!/bin/sh
 
 # Author:  Boris Pek <tehnick-8@yandex.ru>
 # License: Public Domain
 # Created: 2011-11-26
-# Updated: 2019-03-07
+# Updated: 2019-04-07
 # Version: N/A
 
 set -e
 
 export CUR_DIR="$(dirname $(realpath -s ${0}))"
-export MAIN_DIR="${CUR_DIR}/.."
+export MAIN_DIR="$(realpath -s ${CUR_DIR}/..)"
 export LANG_DIR="${MAIN_DIR}/eiskaltdcpp_transifex"
 
 cd "${CUR_DIR}"
 
 case "${1}" in
 "up")
+    # Pulling changes from GitHub repo.
 
     git pull --all
 
 ;;
 "cm")
+    # Creating correct git commit.
 
-    git commit -a -m 'Translations are updated from Transifex.'
+    git commit -a -m 'Translations were updated from Transifex.'
 
 ;;
 "make")
+    # Making precompiled localization files.
 
     if [ -d "${CUR_DIR}/builddir" ]; then
         cd "${CUR_DIR}/builddir"
         make eiskaltdcpp-qt_tr mo-update
     else
         mkdir -p builddir && cd builddir
-        cmake -DUSE_QT=ON -DUSE_GTK=ON ..
+        cmake -DUSE_QT5=ON -DUSE_GTK=ON ..
         make eiskaltdcpp-qt_tr mo-update
     fi
 
 ;;
 "tr")
+    # Pulling changes from Transifex.
 
     # Test Internet connection:
     host transifex.com > /dev/null
@@ -51,7 +55,7 @@ case "${1}" in
     cd "${LANG_DIR}"
     tx pull
 
-    cd "${LANG_DIR}/translations/eiskaltdcpp.dcpp"
+    cd "${LANG_DIR}/translations/eiskaltdcpp.libeiskaltdcpp"
     cp *.po "${CUR_DIR}/dcpp/po/"
 
     cd "${LANG_DIR}/translations/eiskaltdcpp.eiskaltdcpp-gtk"
@@ -60,15 +64,18 @@ case "${1}" in
     cd "${LANG_DIR}/translations/eiskaltdcpp.eiskaltdcpp-qt"
     cp *.ts "${CUR_DIR}/eiskaltdcpp-qt/translations/"
 
+    cd "${LANG_DIR}/translations/eiskaltdcpp.desktop-file"
+    cp *.desktop "${CUR_DIR}/eiskaltdcpp-qt/desktop-file/"
+
     cd "${CUR_DIR}"
     git status
 
 ;;
 "tr_up")
+    # Full update of localization files.
 
     cd "${CUR_DIR}"
 
-    export QT_SELECT=qt4
     lupdate eiskaltdcpp-qt/translations.pro
 
     cd builddir
@@ -83,10 +90,10 @@ case "${1}" in
 
 ;;
 "tr_cl")
+    # Cleaning update of localization files.
 
     cd "${CUR_DIR}"
 
-    export QT_SELECT=qt4
     lupdate -verbose -no-obsolete eiskaltdcpp-qt/translations.pro
 
     cd builddir
@@ -101,16 +108,19 @@ case "${1}" in
 
 ;;
 "tr_push")
+    # Pushing changes to Transifex.
 
     cd "${LANG_DIR}/translations"
 
-    cp "${CUR_DIR}"/dcpp/po/*.po eiskaltdcpp.dcpp/
+    cp "${CUR_DIR}"/dcpp/po/*.po eiskaltdcpp.libeiskaltdcpp/
     cp "${CUR_DIR}"/eiskaltdcpp-gtk/po/*.po eiskaltdcpp.eiskaltdcpp-gtk/
     cp "${CUR_DIR}"/eiskaltdcpp-qt/translations/*.ts eiskaltdcpp.eiskaltdcpp-qt/
+    cp "${CUR_DIR}"/eiskaltdcpp-qt/desktop-file/*.desktop eiskaltdcpp.desktop-file/
 
     cd "${LANG_DIR}"
     if [ -z "${2}" ]; then
-        echo "<arg> is not specified!" ; exit 1
+        echo "<arg> is not specified!"
+        exit 1
     elif [ "${2}" = "src" ] ; then
         tx push -s
     elif [ "${2}" = "all" ] ; then
@@ -121,6 +131,7 @@ case "${1}" in
 
 ;;
 "tr_co")
+    # Cloning Transifex repo.
 
     if [ -d "${LANG_DIR}" ]; then
         echo "\"${LANG_DIR}\" directory already exists!"
@@ -134,11 +145,48 @@ case "${1}" in
     fi
 
 ;;
+"tr_sync")
+    # Syncing Transifex and GitHub repos.
+
+    "${0}" up > /dev/null
+    "${0}" tr > /dev/null
+
+    if [ "$(git status | grep 'l10n/' | wc -l)" -gt 0 ]; then
+        "${0}" cm
+        "${0}" push
+    fi
+    echo ;
+
+;;
+"desktop_up")
+    # Update main .desktop file
+
+    GENERICNAME_FULL_DATA=$(grep -r "GenericName\[" "${CUR_DIR}/eiskaltdcpp-qt/desktop-file/" | grep -v '/en.desktop:')
+    GENERICNAME_FILTERED_DATA=$(echo "${GENERICNAME_FULL_DATA}" | sed -ne 's|^.*/.*.desktop:\(.*\)$|\1|p')
+    GENERICNAME_SORTED_DATA=$(echo "${GENERICNAME_FILTERED_DATA}" | sort -uV)
+
+    COMMENT_FULL_DATA=$(grep -r "Comment\[" "${CUR_DIR}/eiskaltdcpp-qt/desktop-file/" | grep -v '/en.desktop:')
+    COMMENT_FILTERED_DATA=$(echo "${COMMENT_FULL_DATA}" | sed -ne 's|^.*/.*.desktop:\(.*\)$|\1|p')
+    COMMENT_SORTED_DATA=$(echo "${COMMENT_FILTERED_DATA}" | sort -uV)
+
+    DESKTOP_FILE="${CUR_DIR}/eiskaltdcpp-qt/eiskaltdcpp-qt.desktop"
+    grep -v "GenericName\[" "${DESKTOP_FILE}" > "${DESKTOP_FILE}.tmp"
+    mv -f "${DESKTOP_FILE}.tmp" "${DESKTOP_FILE}"
+    grep -v "Comment\[" "${DESKTOP_FILE}" > "${DESKTOP_FILE}.tmp"
+    mv -f "${DESKTOP_FILE}.tmp" "${DESKTOP_FILE}"
+    echo "${GENERICNAME_SORTED_DATA}" >> "${DESKTOP_FILE}"
+    echo "${COMMENT_SORTED_DATA}" >> "${DESKTOP_FILE}"
+
+    # Update .desktop file for English localization
+    cp -f "${DESKTOP_FILE}" "${CUR_DIR}/eiskaltdcpp-qt/desktop-file/en.desktop"
+
+;;
 *)
+    # Help.
 
     echo "Usage:"
     echo "  up cm make"
-    echo "  tr tr_up tr_cl tr_co"
+    echo "  tr tr_up tr_cl tr_co tr_sync desktop_up"
     echo "  tr_push <arg> (arg: src, all or language code)"
     echo ;
     echo "Examples:"
