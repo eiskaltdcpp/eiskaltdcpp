d50640a8015152db5dfabba6ab9893ea1dd1b6bb
Boris Pek <tehnick-8@yandex.ru>
2019-04-08 00:55:05 +0300

    Update documentation.

--- eiskaltdcpp-qt/src/MainWindow.cpp
+++ eiskaltdcpp-qt/src/MainWindow.cpp
@@ -2827,7 +2827,7 @@ void MainWindow::slotAboutClient() {
         QString("&nbsp;&nbsp;&nbsp; 2014 <a href=\"mailto:trifunovic@openmailbox.org\">Marko Trifunović</a><br/>")+
         QString("<br/>")+
         tr("Spanish translation<br/>")+
-        QString("&nbsp;&nbsp;&nbsp; 2010-2018 <a href=\"mailto:sl1pkn07@gmail.com\">Gustavo Alvarez</a> aka sL1pKn07<br/>")+
+        QString("&nbsp;&nbsp;&nbsp; 2010-2019 <a href=\"mailto:sl1pkn07@gmail.com\">Gustavo Alvarez</a> aka sL1pKn07<br/>")+
         QString("&nbsp;&nbsp;&nbsp; 2012-2015 <a href=\"mailto:klondike at klondike.es\">Francisco Blas Izquierdo Riera</a> aka klondike<br/>")+
         QString("<br/>")+
         tr("Basque translation<br/>")+
@@ -2867,7 +2867,7 @@ void MainWindow::slotAboutClient() {
         QString("&nbsp;&nbsp;&nbsp; 2014-2019 <a href=\"mailto:sopor@hotmail.com\">Sopor</a><br/>")+
         QString("<br/>")+
         tr("Turkish translation<br/>")+
-        QString("&nbsp;&nbsp;&nbsp; 2015-2018 <a href=\"https://www.transifex.com/user/profile/mauron/\">mauron</a><br/>")+
+        QString("&nbsp;&nbsp;&nbsp; 2015-2019 <a href=\"https://www.transifex.com/user/profile/mauron/\">mauron</a><br/>")+
         QString("<br/>")
         );
 
