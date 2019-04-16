eiskaltdcpp/eiskaltdcpp

commit daab31b4e99f2a4e2ff2fe36afc03caf55c07fa6

[Core] Code refactoring.

dcpp/NmdcHub.cpp
@@ -323,6 +323,7 @@ void NmdcHub::onLine(const string& aLine) noexcept {
         i = j + 1;
         string terms = unescape(param.substr(i));
 
+        // without terms, this is an invalid search.
         if(!terms.empty()) {
             if(seeker.compare(0, 4, "Hub:") == 0) {
                 OnlineUser* u = findUser(seeker.substr(4));
@@ -557,12 +558,12 @@ void NmdcHub::onLine(const string& aLine) noexcept {
     } else if(cmd == "$Supports") {
         StringTokenizer<string> st(param, ' ');
         StringList& sl = st.getTokens();
-        for(auto i = sl.begin(); i != sl.end(); ++i) {
-            if(*i == "UserCommand") {
+        for(auto& i: sl) {
+            if(i == "UserCommand") {
                 supportFlags |= SUPPORTS_USERCOMMAND;
-            } else if(*i == "NoGetINFO") {
+            } else if(i == "NoGetINFO") {
                 supportFlags |= SUPPORTS_NOGETINFO;
-            } else if(*i == "UserIP2") {
+            } else if(i == "UserIP2") {
                 supportFlags |= SUPPORTS_USERIP2;
             }
         }
@@ -619,13 +620,14 @@ void NmdcHub::onLine(const string& aLine) noexcept {
             }
 
             if(CryptoManager::getInstance()->isExtended(lock)) {
-                StringList feat;
-                feat.push_back("UserCommand");
-                feat.push_back("NoGetINFO");
-                feat.push_back("NoHello");
-                feat.push_back("UserIP2");
-                feat.push_back("TTHSearch");
-                feat.push_back("ZPipe0");
+                StringList feat = {
+                    "UserCommand",
+                    "NoGetINFO",
+                    "NoHello"
+                    "UserIP2",
+                    "TTHSearch",
+                    "ZPipe0"
+                };
 
                 if(CryptoManager::getInstance()->TLSOk())
                     feat.push_back("TLS");
@@ -781,8 +783,12 @@ void NmdcHub::onLine(const string& aLine) noexcept {
             return;
 
         string fromNick = param.substr(i+1, j-i-1);
-        if(fromNick.empty() || param.size() < j + 2)
+        if(fromNick.empty())
             return;
+
+        if(param.size() < j + 2) {
+            return;
+        }
 
         ChatMessage message = { unescape(param.substr(j + 2)), findUser(fromNick), &getUser(getMyNick()), findUser(rtNick), false, 0 };
 

dcpp/AdcHub.h
@@ -57,6 +57,7 @@ public:
     virtual int64_t getAvailable() const;
 
     static string escape(const string& str) { return AdcCommand::escape(str, false); }
+    void emulateCommand(const string& cmd) { dispatch(cmd); }
     virtual void send(const AdcCommand& cmd);
 
     string getMySID() { return AdcCommand::fromSID(sid); }

dcpp/Client.h
@@ -82,9 +82,10 @@ public:
     virtual size_t getUserCount() const = 0;
     virtual int64_t getAvailable() const = 0;
     static int getTotalCounts() { return counts.normal + counts.registered + counts.op; }
-    virtual void send(const AdcCommand& command) = 0;
-
     static string escape(string const& str) { return str; }
+
+    virtual void emulateCommand(const string& cmd) = 0;
+    virtual void send(const AdcCommand& command) = 0;
 
     bool isConnected() const { return state != STATE_DISCONNECTED; }
     bool isReady() const { return state != STATE_CONNECTING && state != STATE_DISCONNECTED; }

dcpp/NmdcHub.h
@@ -69,6 +69,7 @@ public:
     static string escape(const string& str) { return validateMessage(str, false); }
     static string unescape(const string& str) { return validateMessage(str, true); }
 
+    void emulateCommand(const string& cmd) { onLine(cmd); }
     virtual void send(const AdcCommand&) { dcassert(0); }
 
     static string validateMessage(string tmp, bool reverse);

dcpp/QueueManager.cpp
@@ -17,7 +17,6 @@
  */
 
 #include "stdinc.h"
-
 #include "QueueManager.h"
 
 #include "ClientManager.h"
@@ -25,21 +24,21 @@
 #include "DirectoryListing.h"
 #include "Download.h"
 #include "DownloadManager.h"
+#include "FilteredFile.h"
+#include "FinishedItem.h"
+#include "FinishedManager.h"
 #include "HashManager.h"
 #include "LogManager.h"
+#include "MerkleCheckOutputStream.h"
 #include "SearchManager.h"
+#include "SearchResult.h"
+#include "SFVReader.h"
 #include "ShareManager.h"
 #include "SimpleXML.h"
 #include "StringTokenizer.h"
 #include "Transfer.h"
 #include "UserConnection.h"
 #include "version.h"
-#include "SearchResult.h"
-#include "MerkleCheckOutputStream.h"
-#include "SFVReader.h"
-#include "FilteredFile.h"
-#include "FinishedItem.h"
-#include "FinishedManager.h"
 #include "ZUtils.h"
 
 #ifdef WITH_DHT
@@ -92,7 +91,6 @@ QueueItem* QueueManager::FileQueue::add(const string& aTarget, int64_t aSize,
 
     qi->setTempTarget(aTempTarget);
 
-    dcassert(find(aTarget) == NULL);
     add(qi);
     return qi;
 }
@@ -116,24 +114,15 @@ QueueItem* QueueManager::FileQueue::find(const string& target) {
     return (i == queue.end()) ? NULL : i->second;
 }
 
-void QueueManager::FileQueue::find(QueueItem::List& sl, int64_t aSize, const string& suffix) {
-    for(auto& i: queue) {
-        if(i.second->getSize() == aSize) {
-            const string& t = i.second->getTarget();
-            if(suffix.empty() || (suffix.length() < t.length() &&
-                                  Util::stricmp(suffix.c_str(), t.c_str() + (t.length() - suffix.length())) == 0) )
-                sl.push_back(i.second);
-        }
-    }
-}
-
-void QueueManager::FileQueue::find(QueueItem::List& ql, const TTHValue& tth) {
+QueueManager::QueueItemList QueueManager::FileQueue::find(const TTHValue& tth) {
+    QueueItemList ql;
     for(auto& i: queue) {
         auto qi = i.second;
         if(qi->getTTH() == tth) {
             ql.push_back(qi);
         }
     }
+    return ql;
 }
 
 bool QueueManager::FileQueue::exists(const TTHValue& tth) const {
@@ -677,7 +666,8 @@ string QueueManager::getListPath(const HintedUser& user) {
     string nick = nicks.empty() ? Util::emptyString : Util::cleanPathChars(nicks[0]) + ".";
     return checkTarget(Util::getListPath() + nick + user.user->getCID().toBase32(), /*checkExistence*/ false);
 }
-//NOTE: freedcpp
+
+// NOTE: freedcpp: begin
 void QueueManager::add(const string& aTarget, int64_t aSize, const TTHValue& root)
 {
     // Check if we're not downloading something already in our share
@@ -708,8 +698,7 @@ void QueueManager::add(const string& aTarget, int64_t aSize, const TTHValue& roo
     // This will be pretty slow on large queues...
     if (BOOLSETTING(DONT_DL_ALREADY_QUEUED))
     {
-        QueueItem::List ql;
-        fileQueue.find(ql, root);
+        auto ql = fileQueue.find(root);
         if (!ql.empty())
             throw QueueException(_("This file is already queued"));
     }
@@ -731,7 +720,8 @@ void QueueManager::add(const string& aTarget, int64_t aSize, const TTHValue& roo
             throw QueueException(_("A file with different tth root already exists in the queue"));
         }
     }
-}//NOTE: freedcpp
+}
+// NOTE: freedcpp: end
 
 void QueueManager::add(const string& aTarget, int64_t aSize, const TTHValue& root, const HintedUser& aUser,
                        int aFlags /* = 0 */, bool addBad /* = true */)
@@ -773,15 +763,14 @@ void QueueManager::add(const string& aTarget, int64_t aSize, const TTHValue& roo
 
         // This will be pretty slow on large queues...
         if(BOOLSETTING(DONT_DL_ALREADY_QUEUED) && !(aFlags & QueueItem::FLAG_USER_LIST)) {
-            QueueItem::List ql;
-            fileQueue.find(ql, root);
+            auto ql = fileQueue.find(root);
             if (!ql.empty()) {
                 // Found one or more existing queue items, lets see if we can add the source to them
                 bool sourceAdded = false;
-                for(QueueItem::Iter i = ql.begin(); i != ql.end(); ++i) {
-                    if(!(*i)->isSource(aUser)) {
+                for(auto& i : ql) {
+                    if(!i->isSource(aUser)) {
                         try {
-                            wantConnection = addSource(*i, aUser, addBad ? QueueItem::Source::FLAG_MASK : 0);
+                            wantConnection = addSource(i, aUser, addBad ? QueueItem::Source::FLAG_MASK : 0);
                             sourceAdded = true;
                         } catch(...) { }
                     }
@@ -1047,13 +1036,14 @@ void QueueManager::move(const string& aSource, const string& aTarget) noexcept {
     }
 }
 
-void QueueManager::getTargets(const TTHValue& tth, StringList& sl) {
+StringList QueueManager::getTargets(const TTHValue& tth) {
     Lock l(cs);
-    QueueItem::List ql;
-    fileQueue.find(ql, tth);
+    auto ql = fileQueue.find(tth);
+    StringList sl;
     for(auto& i: ql) {
         sl.push_back(i->getTarget());
     }
+    return sl;
 }
 
 Download* QueueManager::getDownload(UserConnection& aSource, bool supportsTrees) noexcept {
@@ -1750,7 +1740,7 @@ void QueueLoader::startTag(const string& name, StringPairList& attribs, bool sim
     if(!inDownloads && name == "Downloads") {
         inDownloads = true;
     } else if(inDownloads) {
-        if(cur == NULL && name == sDownload) {
+        if(cur == nullptr && name == sDownload) {
             int64_t size = Util::toInt64(getAttrib(attribs, sSize, 1));
             if(size == 0)
                 return;
@@ -1779,7 +1769,7 @@ void QueueLoader::startTag(const string& name, StringPairList& attribs, bool sim
 
             QueueItem* qi = qm->fileQueue.find(target);
 
-            if(qi == NULL) {
+            if(qi == nullptr) {
                 qi = qm->fileQueue.add(target, size, 0, p, tempTarget, added, TTHValue(tthRoot));
                 if(downloaded > 0) {
                     qi->addSegment(Segment(0, downloaded));
@@ -1821,7 +1811,7 @@ void QueueLoader::startTag(const string& name, StringPairList& attribs, bool sim
 void QueueLoader::endTag(const string& name, const string&) {
     if(inDownloads) {
         if(name == sDownload) {
-            cur = NULL;
+            cur = nullptr;
         } else if(name == "Downloads") {
             inDownloads = false;
         }
@@ -1841,13 +1831,9 @@ void QueueManager::on(SearchManagerListener::SR, const SearchResultPtr& sr) noex
 
     {
         Lock l(cs);
-        QueueItem::List matches;
+        auto matches = fileQueue.find(sr->getTTH());
 
-        fileQueue.find(matches, sr->getTTH());
-
-        for(QueueItem::Iter i = matches.begin(); i != matches.end(); ++i) {
-            QueueItem* qi = *i;
-
+        for(auto qi : matches) {
             // Size compare to avoid popular spoof
             if(qi->getSize() == sr->getSize() && !qi->isSource(sr->getUser())) {
                 try {
@@ -1922,15 +1908,14 @@ bool QueueManager::handlePartialResult(const UserPtr& aUser, const string& hubHi
         Lock l(cs);
 
         // Locate target QueueItem in download queue
-        QueueItem::List ql;
-        fileQueue.find(ql, tth);
+        auto ql = fileQueue.find(tth);
 
         if(ql.empty()){
             dcdebug("Not found in download queue\n");
             return false;
         }
 
-        QueueItem::Ptr qi = ql[0];
+        QueueItemPtr qi = ql[0];
         // don't add sources to finished files
         // this could happen when "Keep finished files in queue" is enabled
         if(qi->isFinished())
@@ -1996,14 +1981,13 @@ bool QueueManager::handlePartialSearch(const TTHValue& tth, PartsInfo& _outParts
         Lock l(cs);
 
         // Locate target QueueItem in download queue
-        QueueItem::List ql;
-        fileQueue.find(ql, tth);
+        auto ql = fileQueue.find(tth);
 
         if(ql.empty()){
             return false;
         }
 
-        QueueItem::Ptr qi = ql[0];
+        QueueItemPtr qi = ql[0];
         if(qi->getSize() < PARTIAL_SHARE_MIN_SIZE){
             return false;
         }

dcpp/QueueManager.h
@@ -17,20 +17,24 @@
 
 #pragma once
 
-#include "TimerManager.h"
+#include <deque>
+
+#include "ClientManagerListener.h"
 #include "CriticalSection.h"
-#include "Exception.h"
-#include "User.h"
-#include "File.h"
-#include "QueueItem.h"
-#include "Singleton.h"
 #include "DirectoryListing.h"
+#include "Exception.h"
+#include "File.h"
 #include "MerkleTree.h"
+#include "QueueItem.h"
 #include "QueueManagerListener.h"
 #include "SearchManagerListener.h"
-#include "ClientManagerListener.h"
+#include "Singleton.h"
+#include "TimerManager.h"
+#include "User.h"
 
 namespace dcpp {
+
+using std::deque;
 
 STANDARD_EXCEPTION(QueueException);
 
@@ -68,8 +72,9 @@ class QueueManager : public Singleton<QueueManager>, public Speaker<QueueManager
         private SearchManagerListener, private ClientManagerListener
 {
 public:
-    //NOTE: freedcpp
-    void add(const string& aTarget, int64_t aSize, const TTHValue& root);
+    typedef deque<QueueItemPtr> QueueItemList;
+
+    void add(const string& aTarget, int64_t aSize, const TTHValue& root); // NOTE: freedcpp
 
     /** Add a file to the queue. */
     void add(const string& aTarget, int64_t aSize, const TTHValue& root, const HintedUser& aUser,
@@ -101,7 +106,7 @@ public:
 
     void setPriority(const string& aTarget, QueueItem::Priority p) noexcept;
 
-    void getTargets(const TTHValue& tth, StringList& sl);
+    StringList getTargets(const TTHValue& tth);
     QueueItem::StringMap& lockQueue() noexcept { cs.lock(); return fileQueue.getQueue(); }
     void unlockQueue() noexcept { cs.unlock(); }
 
@@ -128,8 +133,7 @@ public:
 
     bool isChunkDownloaded(const TTHValue& tth, int64_t startPos, int64_t& bytes, string& tempTarget, int64_t& size) {
         Lock l(cs);
-        QueueItem::List ql;
-        fileQueue.find(ql, tth);
+        auto ql = fileQueue.find(tth);
 
         if(ql.empty()) return false;
 
@@ -199,8 +203,7 @@ private:
                        const string& aTempTarget, time_t aAdded, const TTHValue& root);
 
         QueueItem* find(const string& target);
-        void find(QueueItem::List& sl, int64_t aSize, const string& ext);
-        void find(QueueItem::List& ql, const TTHValue& tth);
+        QueueManager::QueueItemList find(const TTHValue& tth);
         // find some PFS sources to exchange parts info
         void findPFSSources(PFSSourceList&);
         bool exists(const TTHValue& tth) const;

dcpp/forward.h
@@ -73,6 +73,7 @@ class OnlineUser;
 typedef OnlineUser* OnlineUserPtr;
 
 class QueueItem;
+typedef QueueItem* QueueItemPtr;
 
 class SearchResult;
 typedef boost::intrusive_ptr<SearchResult> SearchResultPtr;

eiskaltdcpp-gtk/src/search.cc
@@ -391,8 +391,7 @@ void Search::popupMenu_gui()
 
     if (hasTTH)
     {
-        StringList targets;
-        QueueManager::getInstance()->getTargets(TTHValue(tth), targets);
+        StringList targets = QueueManager::getInstance()->getTargets(TTHValue(tth));
 
         if (!targets.empty())
         {

commit 2a6ca36ce96f390917b9b48a3f1b582b86880081

[Core] Fix typo introduced in daab31b4.

dcpp/NmdcHub.cpp
@@ -625,7 +625,7 @@ void NmdcHub::onLine(const string& aLine) noexcept {
                 StringList feat = {
                     "UserCommand",
                     "NoGetINFO",
-                    "NoHello"
+                    "NoHello",
                     "UserIP2",
                     "TTHSearch",
                     "ZPipe0"