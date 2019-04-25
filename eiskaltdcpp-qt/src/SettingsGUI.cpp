8b8ed6220005573d29c5b17727581d3051fe0867
Boris Pek <tehnick-8@yandex.ru>
2019-04-25 23:05:27 +0300

    [Qt] Allow to save empty path to language file in Preferences dialog.

--- eiskaltdcpp-qt/src/SettingsGUI.cpp
+++ eiskaltdcpp-qt/src/SettingsGUI.cpp
@@ -369,8 +369,7 @@ void SettingsGUI::ok(){
         if (!lineEdit_APPFONT->text().isEmpty())
             WSSET(WS_APP_FONT, lineEdit_APPFONT->text());
 
-        if (!lineEdit_LANGFILE->text().isEmpty() && (lineEdit_LANGFILE->text() != WSGET(WS_TRANSLATION_FILE)))
-            WSSET(WS_TRANSLATION_FILE, lineEdit_LANGFILE->text());
+        WSSET(WS_TRANSLATION_FILE, lineEdit_LANGFILE->text());
 
         WBSET(WB_MAINWINDOW_REMEMBER, radioButton_REMEMBER->isChecked());
         WBSET(WB_MAINWINDOW_HIDE, radioButton_HIDE->isChecked());
