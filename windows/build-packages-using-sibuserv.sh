ebeaa44eb4e9da375d0f2d796579ff6c9056a441
Boris Pek <tehnick-8@yandex.ru>
2019-05-21 00:08:05 +0300

    Windows: switch build script to static builds.

--- windows/build-packages-using-sibuserv.sh
+++ windows/build-packages-using-sibuserv.sh
@@ -3,7 +3,7 @@
 # Author:  Boris Pek <tehnick-8@yandex.ru>
 # License: MIT (Expat)
 # Created: 2019-04-01
-# Updated: 2019-05-16
+# Updated: 2019-05-21
 # Version: N/A
 #
 # Dependencies:
@@ -19,8 +19,8 @@ CUR_DIR="$(dirname $(realpath -s ${0}))"
 . "${CUR_DIR}/downloads_library.sh"
 . "${CUR_DIR}/common_functions.sh"
 
-BUILD_TARGETS="i686-w64-mingw32.shared x86_64-w64-mingw32.shared"
-#BUILD_TARGETS="i686-w64-mingw32.static x86_64-w64-mingw32.static"
+#BUILD_TARGETS="i686-w64-mingw32.shared x86_64-w64-mingw32.shared"
+BUILD_TARGETS="i686-w64-mingw32.static x86_64-w64-mingw32.static"
 
 # Script body
 
