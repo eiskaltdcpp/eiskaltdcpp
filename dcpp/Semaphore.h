4a3131cd44922c3e28761fdf868a05f0ac9e717c
Boris Pek <tehnick-8@yandex.ru>
2021-01-24 14:03:50 +0300

    Update years in documentation.

--- dcpp/Semaphore.h
+++ dcpp/Semaphore.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2001-2012 Jacek Sieka, arnetheduck on gmail point com
- * Copyright (C) 2009-2019 EiskaltDC++ developers
+ * Copyright (C) 2009-2020 EiskaltDC++ developers
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
