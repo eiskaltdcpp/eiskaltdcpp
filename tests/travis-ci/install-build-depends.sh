7ce88409850b3a1a46ee9b26350f4e864beea242
Boris Pek <tehnick-8@yandex.ru>
2021-04-24 18:42:23 +0300

    Travis CI: improve installation script

--- tests/travis-ci/install-build-depends.sh
+++ tests/travis-ci/install-build-depends.sh
@@ -61,8 +61,8 @@ fi
 if [ "${TARGET}" = "windows32" ] || [ "${TARGET}" = "windows64" ]
 then
     # Add debian packages built from MXE packages
-    sudo add-apt-repository 'deb [arch=amd64] https://mirror.mxe.cc/repos/apt focal main'
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C6BF758A33A3A276
+    sudo add-apt-repository 'deb [arch=amd64] https://mirror.mxe.cc/repos/apt focal main'
 
     if [ "${TARGET}" = "windows64" ]
     then
